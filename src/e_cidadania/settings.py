d27672d055fef0273f35ca45821256977ac742d1
Oscar Carballal Prego <info@oscarcp.com>
2010-12-15 13:43:26 +0100

    Settings file configured

--- src/e_cidadania/settings.py
+++ src/e_cidadania/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecidadania',
+        'USER': 'root',
+        'PASSWORD': 'cidadania400',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -27,11 +27,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Madrid'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
 SITE_ID = 1
 
@@ -78,9 +78,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'e_cidadania.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    '/home/oscar.carballal/devel/e_cidadania/src/e_cidadania/templates'
 )
 
 INSTALLED_APPS = (
@@ -89,8 +87,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    
+    'e_cidadania.accounts',
+    'e_cidadania.debate',
 )
+
+AUTH_PROFILE_MODULE = "accounts.UserProfile"
