d8bc501023ed840edacbf932f1b0e8f84888f696
Oscar Carballal Prego <info@oscarcp.com>
2011-01-05 14:26:59 +0100

    Added urls.py to the news application and started to code views

---
+++ src/e_cidadania/apps/news/urls.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2010 Cidadanía Coop.
+# Written by: Oscar Carballal Prego <info@oscarcp.com>
+#
+# This file is part of e-cidadania.
+#
+# e-cidadania is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# e-cidadania is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with e-cidadania. If not, see <http://www.gnu.org/licenses/>.
+
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+urlpatterns = patterns('apps.news.views',
+    
+    (r'^add/', 'add_post'),
+    
+    (r'^delete/<post_id>/', 'delete_post'),
+    
+    (r'^edit/<post_id>', 'edit_post')
+
+)
--- src/e_cidadania/apps/news/views.py
+++ src/e_cidadania/apps/news/views.py
@@ -1,1 +1,43 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2010 Cidadanía Coop.
+# Written by: Oscar Carballal Prego <info@oscarcp.com>
+#
+# This file is part of e-cidadania.
+#
+# e-cidadania is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# e-cidadania is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with e-cidadania. If not, see <http://www.gnu.org/licenses/>.
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic.list_detail import object_list
+from django.views.generic.list_detail import object_detail
+from django.views.generic.create_update import create_object
+from django.views.generic.create_update import update_object
+from django.views.generic.create_update import delete_object
+
+from django.contrib.auth.models import User
+from e_cidadania.apps.news.models import Post
+
+def add_post(request):
+    
+    """
+    Create a new post.
+    """
+    return create_object(request,
+                         model = Post,
+                         login_required = True,
+                         template_name = 'news/add_post.html',
+                         post_save_redirect = '/')
