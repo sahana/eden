bfeff74b906855ccc769fe08cd504a891e4ef7b2
Oscar Carballal Prego <info@oscarcp.com>
2011-04-01 14:20:14 +0200

    Added a timeline view and some minor fixes to the template

---
+++ web/js/dates.js
@@ -0,0 +1,72 @@
+var timeline_data = {  // save as a global variable
+'dateTimeFormat': 'iso8601',
+'wikiURL': "http://simile.mit.edu/shelf/",
+'wikiSection': "Simile Cubism Timeline",
+
+'events' : [
+        {'start': '2010-03-01 09:00:00',
+        'title': 'Cidadania echa en falta una herramienta',
+        'description': 'Cidadania echa en falta una herramienta en internet para apoyar los procesos de participación',
+        },
+
+
+        {'start': '2010-04-01 09:00:00',
+        'title': 'Cidadania contacta con Algueirada',
+        'description': 'Cidadania contacta con Algueirada para analizar qué soluciones están disponibles',
+        },
+
+
+        {'start': '2010-05-01 09:00:00',
+        'end': '2010-10-01 09:00:00',
+        'title': 'Inicio de un estudio de e-participación',
+        'description': 'Iniciamos un estudio de las diferentes herramientas de e-participación, con el apoyo de la Consellería de Traballo de la Xunta de Galicia',
+        'isDuration' : true,
+        'color' : 'red',
+        'textColor' : 'green'},
+        
+        {'start': '2010-06-01 09:00:00',
+        'title': 'Descubrimos esfuerzos semejantes',
+        'description': 'En el estudio descubrimos esfuerzos semejantes que nos son de mucha utilidad',
+        },
+        
+        {'start': '2010-10-01 09:00:00',
+        'title': 'Finalizamos el estudio',
+        'description': 'Acabamos el estudio: ahora sabemos la herramienta que queremos, y sabemos que tenemos que construirla casi desde cero',
+        },
+        
+        {'start': '2010-12-01 09:00:00',
+        'title': 'Incorporación de un programador',
+        'description': 'Incorporamos un técnico desarrollador informático, gracias a la colaboración de la Fundación Nova Caixa Galicia',
+        },
+
+        {'start': '2011-01-01 09:00:00',
+        'title': 'Implemntación del sistema de usuarios',
+        'description': 'Ultimamos los trabajos de puesta en marcha de procesos y gestión de usuarios y usuarias',
+        },
+
+        {'start': '2011-02-01 09:00:00',
+        'title': 'Planificación de desarrollo',
+        'description': 'Programamos las fases de desarrollo para ir testando la herramienta en el proceso  de presupuestos participativos de Ferrol',
+        },
+        
+        {'start': '2011-03-01 09:00:00',
+        'title': 'Desarrollo del módulo de debates',
+        'description': 'Se comienza el desarrollo del módulo de debates con la colaboración de Algueirada',
+        },
+        
+        {'start': '2011-04-01 09:00:00',
+        'title': 'Finalización del módulo de propuestas',
+        'description': 'Se termina de desarrollar el módulo de propuestas por parte de Cidadania',
+        },
+        
+        {'start': '2011-05-01 09:00:00',
+        'title': 'El CESGA se incorpora',
+        'description': 'Se incorpora el trabajo de control externo en accesibilidad y errores, por parte del CESGA, Centro de Supercomputación de Galicia',
+        },
+        
+        {'start': '2011-06-01 09:00:00',
+        'title': 'Incorporación de mejoras',
+        'description': 'Incorporación de mejoras de las dos fuentes de contraste: la técnica y la que nos viene de los presupuestos participativos de Ferrol, donde se constituye una comisión de e-participación formada por internatutas de la ciudad',
+        },
+]
+}
---
+++ web/js/local_data.js
@@ -0,0 +1,108 @@
+var timeline_data = {  // save as a global variable
+'dateTimeFormat': 'iso8601',
+'wikiURL': "http://simile.mit.edu/shelf/",
+'wikiSection': "Simile Cubism Timeline",
+
+'events' : [
+        {'start': '1924',
+        'title': 'Barfusserkirche',
+        'description': 'by Lyonel Feininger, American/German Painter, 1871-1956',
+        'image': 'http://images.allposters.com/images/AWI/NR096_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Barfusserkirche-1924-Posters_i1116895_.htm'
+        },
+
+
+        {'start': '2013',
+        'end': '2029',
+        'title': 'Three Figures',
+        'description': 'by Kasimir Malevich, Ukrainian Painter, 1878-1935',
+        'image': 'http://images.allposters.com/images/BRGPOD/75857_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Three-Figures-1913-28-Posters_i1349989_.htm'
+        },
+
+
+        {'start': '2081',
+        'end': '2153',
+        'title': 'Landschaft bei Montreuil',
+        'description': 'by Albert Gleizes, French Painter, 1881-1953',
+        'image': 'http://images.allposters.com/images/mer/1336_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Landschaft-bei-Montreuil-Posters_i339007_.htm',
+        'isDuration' : true,
+        'icon' : "dark-red-circle.png",        
+        'color' : 'red',
+        'textColor' : 'green'},
+
+
+        {'start': '2020',
+        'title': 'Femme au Miroir',
+        'description': 'by Fernand Leger, French Painter, 1881-1955',
+        'image': 'http://images.allposters.com/images/AWI/GMR117_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Femme-au-Miroir-1920-Posters_i141266_.htm'
+        },
+
+
+        {'start': '2003',
+        'title': 'The Old Guitarist',
+        'description': 'by Pablo Picasso, Spanish Painter/Sculptor, 1881-1973',
+        'image': 'http://images.allposters.com/images/ESC/AP599_b.jpg',
+        'link': 'http://www.allposters.com/-sp/The-Old-Guitarist-c-1903-Posters_i328746_.htm'
+        },
+
+
+        {'start': '2082',
+        'end': '2164',
+        'title': 'Jour',
+        'description': 'by Georges Braque, French Painter, 1882-1963',
+        'image': 'http://images.allposters.com/images/SHD/S1041_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Jour-Posters_i126663_.htm',
+        'color': 'green'
+        },
+
+
+        {'start': '1916',
+        'title': 'Still Life with a White Dish',
+        'description': 'by Gino Severini, Italian Painter, 1883-1966',
+        'image': 'http://images.allposters.com/images/MCG/FS1254_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Still-Life-with-a-White-Dish-1916-Posters_i366823_.htm'
+        },
+
+
+        {'start': '2085',
+        'end': '2141',
+        'title': 'Rhythm, Joie de Vivre',
+        'description': 'by Robert Delaunay, French Painter, 1885-1941',
+        'image': 'http://imagecache2.allposters.com/images/pic/adc/10053983a_b~Rhythm-Joie-de-Vivre-Posters.jpg',
+        'link': 'http://www.allposters.com/-sp/Rhythm-Joie-de-Vivre-Posters_i1250641_.htm'
+        },
+
+
+        {'start': '2085',
+        'end': '2125',
+        'title': 'Castor Et Pollux',
+        'description': 'by Roger de la Fresnaye, French Painter, 1885-1925',
+        'image': 'http://images.allposters.com/images/CORPOD/IX001463_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Castor-Et-Pollux-Posters_i831718_.htm',
+        'tapeImage': 'blue_stripes.png',
+        'tapeRepeat': 'repeat-x',
+        'caption': "This is the event's caption attribute.",
+        'classname': 'hot_event' 
+        },
+
+
+        {'start': '1912',
+        'title': 'Portrait of Pablo Picasso',
+        'description': 'by Juan Gris, Spanish Painter/Sculptor, 1887-1927',
+        'image': 'http://images.allposters.com/images/BRGPOD/156514_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Portrait-of-Pablo-Picasso-1881-1973-1912-Posters_i1344154_.htm'
+        },
+
+
+        {'start': '2091',
+        'end': '2115',
+        'title': 'Portrait of Horace Brodsky',
+        'description': 'by Henri Gaudier-Brzeska, French Sculptor, 1891-1915',
+        'image': 'http://imagecache2.allposters.com/images/BRGPOD/102770_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Portrait-of-Horace-Brodsky-Posters_i1584413_.htm'
+        }
+]
+}
