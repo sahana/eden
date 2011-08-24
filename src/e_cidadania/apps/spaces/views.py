20e996441eced1381b003631529b0c5ae4d7fa5b
Oscar Carballal Prego <info@oscarcp.com>
2011-08-24 10:39:11 +0200

    Removed self reference

--- src/e_cidadania/apps/spaces/views.py
+++ src/e_cidadania/apps/spaces/views.py
@@ -313,7 +313,7 @@ def create_space(request):
                 # We add the created spaces to the user allowed spaces
     
                 request.user.profile.spaces.add(space)
-                messages.success(self.request, _('Space %s created successfully.') % space.name)
+                messages.success(request, _('Space %s created successfully.') % space.name)
                 return redirect('/spaces/' + space.url)
     
         return render_to_response('spaces/space_add.html',
