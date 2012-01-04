edd11850d034e1ee5af34c7e8b27ae27a4f69bf5
Dominic König <dominic@nursix.org>
2012-01-04 14:49:11 +0100

    Import Eden modules from main script to have them auto-reloaded in debug mode.

--- models/00_tables.py
+++ models/00_tables.py
@@ -9,9 +9,16 @@
 #
 from s3.s3model import S3Model
 import eden as models
-
 current.models = models
 current.s3db = s3db = S3Model()
+
+# Import Eden modules:
+import eden.pr
+import eden.sit
+import eden.doc
+import eden.project
+import eden.dvi
+import eden.irs
 
 # =============================================================================
 # Import S3 meta fields into global namespace
