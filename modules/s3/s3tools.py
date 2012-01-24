95d113488b4883864b50eb06b02d087eaf9a0ed4
Dominic König <dominic@nursix.org>
2012-01-24 21:39:58 +0100

    Bug fix bulk importer.

--- modules/s3/s3tools.py
+++ modules/s3/s3tools.py
@@ -423,8 +423,7 @@ class S3BulkImporter(object):
             # Store the view
             view = response.view
 
-            auth = current.auth
-            deployment_settings = auth.deployment_settings
+            deployment_settings = current.deployment_settings
 
             _debug ("Running job %s %s (filename=%s transform=%s)" % (task[1], task[2], task[3], task[4]))
             prefix = task[1]
