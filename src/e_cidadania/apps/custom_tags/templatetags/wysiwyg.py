b2190f58d1f7e5dbd81563a09bfed337251b2155
he116ra1n <hugo.deslongchamps@gmail.com>
2012-04-09 17:02:42 +0200

    commit before checkout

--- src/e_cidadania/apps/custom_tags/templatetags/wysiwyg.py
+++ src/e_cidadania/apps/custom_tags/templatetags/wysiwyg.py
@@ -1,8 +1,25 @@
+from urlparse import urljoin
+
 from django import template
+from django.conf import settings
+from django.template import Context, Template
+from django.template.defaultfilters import stringfilter
+from django.template.loader import get_template, render_to_string
 
 register = template.Library()
 
-def wysiwyg_editor(parser):
-	return {'Title': "Edit"}
+@register.simple_tag
+def wysiwyg_editor(field_id, editor_name=None, config=None):
+	if not editor_name:
+		editor_name = "%s_editor" % field_id
 
-register.inclusion_tag('wysiwyg.html')(wysiwyg_editor)
+	ctx = {
+		'field_id': field_id,
+		'editor_name': editor_name,
+		'config': config
+	}
+
+	return render_to_string(
+		"../templates/wysihtml5_instance.html",
+		ctx
+)
