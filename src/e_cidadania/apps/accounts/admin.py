87645e5ec55c435bd860cfcc5b68f09c97676446
Oscar Carballal Prego <info@oscarcp.com>
2012-04-19 14:00:29 +0200

    Added mass mailing system

--- src/e_cidadania/apps/accounts/admin.py
+++ src/e_cidadania/apps/accounts/admin.py
@@ -21,6 +21,8 @@
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
 
 from e_cidadania.apps.accounts.models import UserProfile
 
@@ -31,14 +33,24 @@ class ProfileAdmin(admin.ModelAdmin):
     user and the website.
     """
     list_display = ('user', 'website')
+    actions = ['mass_mail']
     
     def mass_mail(self, request, queryset):
         """
         This function exports the selected ovjects to a new view to manipulate
         them properly.
         """
+        #selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        # ct = ContentType.objects.get_for_model(queryset.model)
+        if request.method == "POST":
+            for obj in queryset:
+                get_user = get_object_or_404(User, id=obj.id)
+                send.mail(request.POST['massmail_subject'], request.POST['message'], 'noreply@ecidadania.org', [get_user.email])
+            return HttpResponseRedirect(request.get_full_path())
+        
         selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
         ct = ContentType.objects.get_for_model(queryset.model)
-        return HttpResponseRedirect('/mail/%s/%s/' % (ct.pk, ','.join(selected)))
+        return render_to_response('/mail/massmail.html', { 'people': selected })
+    mass_mail.short_description = 'Send a global mail to the selected users'
     
 admin.site.register(UserProfile, ProfileAdmin)
