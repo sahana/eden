c4cc9cb1c6d60d3982754734d85ca5d2f89774c2
Hugo Des <hugo.deslongchamps@gmail.com>
2012-04-26 11:42:28 +0200

    Added smart_selects to the right folder

---
+++ src/apps/thirdparty/smart_selects/db_fields.py
@@ -0,0 +1,74 @@
+from django.db.models.fields.related import ForeignKey
+import form_fields
+try:
+    from south.modelsinspector import add_introspection_rules
+    has_south = True
+except:
+    has_south = False
+
+
+class ChainedForeignKey(ForeignKey):
+    """
+    chains the choices of a previous combo box with this one
+    """
+    def __init__(self, to, chained_field=None, chained_model_field=None, show_all=False, auto_choose=False, **kwargs):
+        if isinstance(to, basestring):
+            self.app_name, self.model_name = to.split('.')
+        else:
+            self.app_name = to._meta.app_label
+            self.model_name = to._meta.object_name
+        self.chain_field = chained_field
+        self.model_field = chained_model_field
+        self.show_all = show_all
+        self.auto_choose = auto_choose
+        ForeignKey.__init__(self, to, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': form_fields.ChainedModelChoiceField,
+            'queryset': self.rel.to._default_manager.complex_filter(self.rel.limit_choices_to),
+            'to_field_name': self.rel.field_name,
+            'app_name': self.app_name,
+            'model_name': self.model_name,
+            'chain_field': self.chain_field,
+            'model_field': self.model_field,
+            'show_all':self.show_all,
+            'auto_choose':self.auto_choose,
+        }
+        defaults.update(kwargs)
+        return super(ChainedForeignKey, self).formfield(**defaults)
+
+
+
+class GroupedForeignKey(ForeignKey):
+    """
+    Opt Grouped Field
+    """
+    def __init__(self, to, group_field, **kwargs):
+        self.group_field = group_field
+        self._choices = True
+        ForeignKey.__init__(self, to, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': form_fields.GroupedModelSelect,
+            'queryset': self.rel.to._default_manager.complex_filter(
+                                                    self.rel.limit_choices_to),
+            'to_field_name': self.rel.field_name,
+            'order_field': self.group_field,
+        }
+        defaults.update(kwargs)
+        return super(ForeignKey, self).formfield(**defaults)
+
+if has_south:
+    rules_grouped = [(
+        (GroupedForeignKey,),
+        [],
+        {
+            'to': ['rel.to', {}],
+            'group_field': ['group_field', {}],
+        },
+    )]
+
+    add_introspection_rules([], ["^smart_selects\.db_fields\.ChainedForeignKey"])
+    add_introspection_rules(rules_grouped, ["^smart_selects\.db_fields\.GroupedForeignKey"])
