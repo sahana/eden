c84875c12efb8f15cf105a35061eec8cd94e3a6b
Dominic König <dominic@nursix.org>
2012-05-14 14:03:10 +0200

    Remove unused update_staff_role.

--- modules/eden/hrm.py
+++ modules/eden/hrm.py
@@ -325,75 +325,6 @@ class S3HRModel(S3Model):
 
     # -------------------------------------------------------------------------
     @staticmethod
-    def hrm_update_staff_role(record, user_id):
-        """
-            Set/retract the Org/Site staff role
-
-            @param record: the hrm_human_resource record
-            @param user_id: the auth_user record ID of the person the
-                            record belongs to.
-
-            @todo: rewrite for new OrgAuth model
-            @todo: combine with pr_human_resource_update_affiliation?
-        """
-
-        # @todo: invalid action, rewrite for new model
-        return
-
-        #db = current.db
-        #auth = current.auth
-
-        #htable = db.hrm_human_resource
-        #utable = auth.settings.table_user
-        #mtable = auth.settings.table_membership
-
-        #org_role = record.owned_by_organisation
-
-        #if record.deleted:
-            #try:
-                #fk = json.loads(record.deleted_fk)
-                #organisation_id = fk.get("organisation_id", None)
-                #site_id = fk.get("site_id", None)
-                #person_id = fk.get("person_id", None)
-            #except:
-                #return
-        #else:
-            #organisation_id = record.get("organisation_id", None)
-            #site_id = record.get("site_id", None)
-            #person_id = record.get("person_id", None)
-
-        #if record.deleted or record.status != 1:
-            #remove_org_role = True
-            #if org_role:
-                #if organisation_id and person_id:
-                    ## Check whether the person has another active
-                    ## HR record in the same organisation
-                    #query = (htable.person_id == person_id) & \
-                            #(htable.organisation_id == organisation_id) & \
-                            #(htable.id != record.id) & \
-                            #(htable.status == 1) & \
-                            #(htable.deleted != True)
-                    #if db(query).select(htable.id, limitby=(0, 1)).first():
-                        #remove_org_role = False
-                #if remove_org_role:
-                    #query = (mtable.user_id == user_id) & \
-                            #(mtable.group_id == org_role)
-                    #db(query).delete()
-            ## @todo:
-            ## Remove from "staff" group (all memberships for the site)
-        #else:
-            #if org_role:
-                #query = (mtable.user_id == user_id) & \
-                        #(mtable.group_id == org_role)
-                #role = db(query).select(limitby=(0, 1)).first()
-                #if not role:
-                    #mtable.insert(user_id = user_id,
-                                  #group_id = org_role)
-            ## @todo:
-            ## Add to "staff" group with restriction to the site
-
-    # -------------------------------------------------------------------------
-    @staticmethod
     def hrm_human_resource_ondelete(row):
         """ On-delete routine for HR records """
 
@@ -422,18 +353,6 @@ class S3HRModel(S3Model):
                 return
 
             s3db.pr_update_affiliations(htable, record)
-
-            query = (ptable.id == person_id) & \
-                    (ltable.pe_id == ptable.pe_id) & \
-                    (utable.id == ltable.user_id)
-            user = db(query).select(utable.id,
-                                    limitby=(0, 1)).first()
-        if not user:
-            return
-        else:
-            user_id = user.id
-            # Set/retract the staff role
-            S3HRModel.hrm_update_staff_role(record, user_id)
 
     # -------------------------------------------------------------------------
     @staticmethod
@@ -464,8 +383,6 @@ class S3HRModel(S3Model):
                                       htable.person_id,
                                       htable.site_id,
                                       htable.organisation_id,
-                                      # Needed by hrm_update_staff_role()
-                                      #htable.owned_by_organisation,
                                       htable.status,
                                       htable.deleted,
                                       htable.deleted_fk,
@@ -529,8 +446,6 @@ class S3HRModel(S3Model):
             if profile:
                 query = (utable.id == user.id)
                 db(query).update(**profile)
-            # Set/retract the staff role
-            S3HRModel.hrm_update_staff_role(record, user_id)
 
         # Ensure only one Facility Contact per Facility
         contact = vars.site_contact
