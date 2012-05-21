af5bd45986e83705fb35778bb29a38960ee243cd
Kunal Hari <hari.kunal@gmail.com>
2012-05-22 00:08:58 +1200

    test system overhaul

--- modules/tests/org/__init__.py
+++ modules/tests/org/__init__.py
@@ -5,7 +5,4 @@ from org_create_office import *
 from org001 import *
 from org002 import *
 from org010 import *
-<<<<<<< HEAD
 >>>>>>> 48e8ff54a739f79d2789eb1dbea7f2bff931ba35
-=======
->>>>>>> a4a139e522f1ee11fd833730bbb4cef2af17bbc2
