c3418e02c112bc940eef0fcff43e7729a8f75ce2
Vivek Hamirwasia <vivsmart@gmail.com>
2012-05-29 01:51:56 +0300

    file traversal

--- TranslationFunctionality/getStrings.py
+++ TranslationFunctionality/getStrings.py
@@ -6,13 +6,9 @@ import symbol
 import token
 
 tflag = 0
-sflag = 0
-mflag = 0
-bracket = 0;
-outstr=''
 
 def parseList(entry,level):
-    global tflag,sflag, bracket,outstr,mflag
+    global tflag
     if isinstance(entry,list):
         id = entry[0]
         value = entry[1]
@@ -21,53 +17,21 @@ def parseList(entry,level):
                 parseList(element, level+1)
         else:
 	    if token.tok_name[id] == "NAME" and value == "T":
-	        sflag = 1
-	    elif sflag == 1:
-	        if token.tok_name[id] == "LPAR":
-		   tflag=1
-		   bracket=1
-	        sflag=0
+	        tflag = 1
 	    elif tflag:
-	         if token.tok_name[id] == "LPAR":
-	               bracket+=1
-	               if bracket>1:
-	                   outstr += '('
-	         elif token.tok_name[id] == "RPAR":
-                       bracket-=1
-	               if bracket>0:
-	                    outstr += ')'
-	               else:
-	                   print entry[2], outstr
-	                   outstr=''
-	                   tflag=0
-	         elif bracket>0:
-	              outstr += value
-            else:
-	       if token.tok_name[id] == "NAME" and value == "M":
-	          mflag = 1
-	       elif mflag == 1:
-	          if token.tok_name[id] == "STRING":
-	             print entry[2], value
-	          elif token.tok_name[id] == "EQUAL" or token.tok_name[id] == "RPAR":
-	              mflag = 0
-
-	       
-                 
+                 if token.tok_name[id] == "STRING" :
+                       print entry[2],value
+                 elif token.tok_name[id] == "RPAR":
+                        tflag = 0
                    
     #        print "%s%s: %s %s" % (" "*level ,token.tok_name[id], value, entry[2])
 
-def _main():
+def findstr(fileName):
     """
       Using the Parse Tree to extract the strings to be translated
     """
-    if len(sys.argv) == 1:
-        print "Please add a python file to process"
-        return
-
     global tflag
     tflag=0
-
-    fileName = sys.argv[1]
 
     try:
         file = open(fileName)
@@ -78,12 +42,12 @@ def _main():
             file = open(fileName)
         except:
             return
+
+    print "\n",fileName,"\n"
+
     fileContent = file.read()
     st = parser.suite(fileContent)
     stList = parser.st2list(st,line_info=1)
-    print fileName
     for element in stList:
         parseList(element, 0)
 
-if __name__ == '__main__':
-    _main()
--- TranslationFunctionality/group_mod.py
+++ TranslationFunctionality/group_mod.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import sys
 import os
-
+import getStrings
 mod = []
+d = {}
+vflag=0
+
 
 def get_module_list():
    
@@ -12,9 +15,65 @@ def get_module_list():
    for f in mod_files:
       cur_file = os.path.join(cont_dir,f)
       mod.append(f[:-3])
- 
+
+def group_files(currentDir,curmod):
+      
+      global d, vflag
+
+      currentDir = os.path.abspath(currentDir)
+      base_dir = os.path.basename(currentDir)
+
+      if base_dir == "views":
+            vflag=1
+      
+
+      files = os.listdir(currentDir)
+      
+      for f in files:
+          curFile = os.path.join(currentDir,f)
+	  if os.path.isdir(curFile):
+
+		  if base_dir=="views":
+		         group_files(curFile,os.path.basename(curFile))
+		  else:
+                         group_files(curFile,curmod)
+	  else:
+		  if vflag==1:
+		     base = curmod
+		  else:
+		     base = os.path.splitext(f)[0]
+
+		  if base_dir=="s3":
+		      base = base[2:]	     
+
+		  if base in mod:
+		     d[base].append(curFile)
+		  else:
+		     d["core"].append(curFile)
+     
+
+
+      if base_dir == "views":
+           vflag=0
+
+
+
 def _main():
+
       get_module_list()
+
+      for m in mod:
+	      d[m] = []
+      d["core"]=[]
+
+      group_files("../","core")
+      for m in d.keys():
+	     print "\n"
+	     print "Module:",m
+	     for f in d[m]:
+	         if f.endswith(".py") == True:
+	                getStrings.findstr(f)
+
 
 if __name__ == '__main__':
    _main()
