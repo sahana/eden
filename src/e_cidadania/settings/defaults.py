10898eaca114152a12ae0be45f7d0dcaa59e1e7a
hugodes <hugo.deslongchamps@gmail.com>
2012-06-09 16:08:45 +0200

    Added GZipMiddleware to settings.defaults
    This will reduce the bandwith used by the server when serving pages

--- src/e_cidadania/settings/defaults.py
+++ src/e_cidadania/settings/defaults.py
@@ -93,6 +93,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
