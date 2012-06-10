7635048ce2335fc50d42e303094a34a27534cf6b
hugodes <hugo.deslongchamps@gmail.com>
2012-06-11 00:19:42 +0200

    Removed some ghost \t chars while reading models.p

--- src/apps/ecidadania/debate/models.py
+++ src/apps/ecidadania/debate/models.py
@@ -36,13 +36,13 @@ class Debate(models.Model):
     Debate object. In every space there can be unlimited debates, each one of
     them holds all the related notes. Debates are filtered by space. Start/End
     dates are for letting users use the debate or not.
-    
+
     .. versionadded:: 0.1b
     """
     title = models.CharField(_('Title'), max_length=200, unique=True)
     description = models.TextField(_('Description'), blank=True, null=True)
     scope = models.CharField(_('Scope'), blank=True, null=True, max_length=100)
-    
+
     space = models.ForeignKey(Space, blank=True, null=True)
     date = models.DateTimeField(_('Date created'), auto_now_add=True)
     date_mod = models.DateTimeField(_('Last update'), auto_now=True)
@@ -50,10 +50,10 @@ class Debate(models.Model):
     start_date = models.DateField(_('Start date'), blank=True, null=True)
     end_date = models.DateField(_('End date'), blank=True, null=True)
 
-    
+
     def __unicode__(self):
         return self.title
-    
+
     def is_active(self):
         if datetime.date.today() >= self.end_date or datetime.date.today() <= self.start_date:
             return False
@@ -70,35 +70,35 @@ class Column(models.Model):
     """
     Debate column object. The debate table is done mixing columns and rows. The column
     object is linked to the debate, but with no preferable order.
-    
+
     .. versionadded:: 0.1b
     """
     criteria = models.CharField(_('Criteria'), max_length=100, blank=True, null=True)
     debate = models.ForeignKey(Debate, blank=True, null=True)
-    
+
     def __unicode__(self):
         return self.criteria
-    
+
 class Row(models.Model):
     """
-    Row object for the debate system.  Thw row object works exactly like the
+    Row object for the debate system.  The row object works exactly like the
     column. It's associated to the debate in no preferred order.
-    
+
     .. versionadded:: 0.1b
     """
     criteria = models.CharField(_('Criteria'), max_length=100, blank=True, null=True)
     debate = models.ForeignKey(Debate, blank=True, null=True)
-    
+
     def __unicode__(self):
         return self.criteria
-           
-    
+
+
 class Note(models.Model):
 
     """
     The most important object in every debate, the message. It has a coordinates
     value to determine the position of the note in its debate.
-    
+
     .. versionadded:: 0.1b
     """
     column = models.ForeignKey(Column, null=True, blank=True)
