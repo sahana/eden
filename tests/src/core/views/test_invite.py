5ae67fdb36d736017713c9c39dd25dc097d7cf63
Praveen Kumar <praveen97uma@gmail.com>
2012-06-13 18:10:30 +0530

    Correct the tests for core.views.invite.

--- tests/src/core/views/test_invite.py
+++ tests/src/core/views/test_invite.py
@@ -38,11 +38,12 @@ class InviteViewTest(ECDTestCase):
         response = self.get(url)
         self.assertResponseOK(response)
         
+        self.create_user('test_user', 'user_pass', logged_in=True)
+        response = self.get(url)
+        #print self.printResponse(response)
+        print response.context['uri']
+        self.assertResponseOK(response)
+        
         post_data = {'email_addr': 'test@gmail.com', 'mail_msg':'test'}
         response = self.post(url, data=post_data)
-        self.assertResponseForbidden(response)
-        
-        #self.create_user('test_user', 'user_pass', logged_in=True)
-        self.create_logged_in_super_user()
-        response = self.post(url, data=post_data)
-        self.assertResponseForbidden(response)
+        self.assertResponseOK(response)
