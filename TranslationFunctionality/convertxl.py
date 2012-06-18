ed72085b2375e250c04c9dc715b44adfd05ac417
Vivek Hamirwasia <vivsmart@gmail.com>
2012-06-18 19:35:18 +0530

    retreiving strings from existing web2py file

--- TranslationFunctionality/convertxl.py
+++ TranslationFunctionality/convertxl.py
@@ -2,6 +2,10 @@
 import sys
 import xlwt
 import group_mod
+import parser
+import symbol
+import token
+import codecs
 
 
 
@@ -9,10 +13,12 @@ def remove_quotes(Strings):
 
 	l = []
 
-	for (loc,data) in Strings:
-            if data[0] == '"' and data[-1] == '"' or data[0] == "'" and data[-1] == "'":
-	            data = data[1:-1]
-            l.append( (loc,data) )
+	for (d1,d2) in Strings:
+            if d1[0] == '"' and d1[-1] == '"' or d1[0] == "'" and d1[-1] == "'":
+	            d1 = d1[1:-1]
+            if d2[0] == '"' and d2[-1] == '"' or d2[0] == "'" and d2[-1] == "'":
+	            d2 = d2[1:-1]
+            l.append( (d1,d2) )
 
         return l
 
@@ -37,10 +43,47 @@ def remove_duplicates(Strings):
     for data in uniq.keys():
         l.append( (uniq[data],data) )
 
-    l.sort( key=lambda tup: tup[1] )
     
     return l
-     
+
+
+
+def parseList(entry,tmpstr):
+
+        if isinstance(entry,list):
+             id = entry[0]
+             value = entry[1]
+             if isinstance(value,list):
+                  for element in entry:
+                       parseList(element,tmpstr)
+             else:
+		     if token.tok_name[id] == "STRING":
+		           tmpstr.append(value)
+		            
+
+
+
+def read_w2pfile(fileName):
+
+	try:
+          file = open(fileName)
+          fileContent = file.read()
+          fileContent = fileContent.replace("\r",'') + '\n'
+          tmpstr=[]
+          st = parser.suite(fileContent)
+          stList = parser.st2list(st,line_info=1)
+          for element in stList:
+             parseList(element,tmpstr)
+
+          strings = []
+          for i in range(0,len(tmpstr)):
+	    if i%2 == 0:
+               strings.append( (tmpstr[i],tmpstr[i+1]) )
+          return strings 
+	except:
+	   return []
+
+       
 
 def create_spreadsheet(Strings):
 
@@ -57,35 +100,54 @@ def create_spreadsheet(Strings):
 
 	row_num = 1
 
-	for (loc,data) in Strings:
+	for (loc,d1,d2) in Strings:
 	    sheet.write(row_num,0,loc,style)
-            sheet.write(row_num,1,data,style)
-	    sheet.write(row_num,2,'',style)
+            sheet.write(row_num,1,unicode(d1, encoding = "utf-8"),style)
+	    sheet.write(row_num,2,unicode(d2, encoding = "utf-8"),style)
 	    row_num+=1
 
 	for colx in range(0,3):
-            sheet.col(colx).width = 14000
+            sheet.col(colx).width = 15000
 
         wbk.save('trans.xls')
 
 
 
 
-def convert_to_xls(modlist,filelist):
+def convert_to_xls(langfile,modlist,filelist):
 
 	group_mod.init()
 
-	Strings = []
+	NewStrings = []
 
 	for mod in modlist:
-	     Strings += group_mod.get_strings_by_module(mod)
+	     NewStrings += group_mod.get_strings_by_module(mod)
         
 	for f in filelist:
-	     Strings += group_mod.get_strings_by_file(f)
+	     NewStrings += group_mod.get_strings_by_file(f)
         
-        Strings = remove_quotes(Strings)
-       
-        Strings = remove_duplicates(Strings)
+        NewStrings = remove_quotes(NewStrings)
+        NewStrings = remove_duplicates(NewStrings)
+        NewStrings.sort( key=lambda tup: tup[1] )
+
+	OldStrings = read_w2pfile(langfile)
+        OldStrings = remove_quotes(OldStrings)
+	OldStrings.sort( key=lambda tup: tup[0] )
+
+
+	Strings = []
+
+	i = 0
+	lim = len(OldStrings)
+
+	for (l,s) in NewStrings:
+	      while i<lim and OldStrings[i][0] < s:
+	          i+=1
+
+	      if i!=lim and OldStrings[i][0] == s:
+	          Strings.append( (l,s,OldStrings[i][1]) )
+	      else:
+	          Strings.append( (l,s,'') )
 
         create_spreadsheet(Strings)
 
@@ -97,8 +159,9 @@ def main():
 	fflag = 0
 	modlist = []
 	filelist = []
+        langfile = sys.argv[1]
 
-	for i in range(1,len(sys.argv)):
+	for i in range(2,len(sys.argv)):
             
 		if sys.argv[i] == "-gsm":
 		    mflag = 1
@@ -112,9 +175,9 @@ def main():
 		    modlist.append(sys.argv[i])
 
 	        elif fflag == 1:
-		   filelist.append(sys.argv[i])
+		    filelist.append(sys.argv[i])
 	   
-        convert_to_xls(modlist,filelist)  
+        convert_to_xls(langfile,modlist,filelist)  
 
 	sys.exit(0)
 
