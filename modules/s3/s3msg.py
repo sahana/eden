883311f2ca97d5204ff72305ef8c79061f040a86
Ashwyn <ashwyn1092@gmail.com>
2012-08-16 14:27:28 +0530

    2-step parser structure.
    Fixes.

--- modules/s3/s3msg.py
+++ modules/s3/s3msg.py
@@ -800,21 +800,26 @@ class S3Msg(object):
            @param: message - Inbound message to be parsed for OpenGeoSMS.
            Returns the lat, lon, code and text contained in the message.
         """
+        
+        lat = ""
+        lon = ""
+        code = ""
+        text = ""
            
         s3db = current.s3db
         words = string.split(message)
         if "http://maps.google.com/maps?q" in words[0]:
             # Parse OpenGeoSMS
-            words = words[0].split("?q=")[1].split(",")
-            lat = words[0]
-            lon = words[1].split("&")[0]
-            code = words[1].split("&")[1].split("=")[1]
+            pwords = words[0].split("?q=")[1].split(",")
+            lat = pwords[0]
+            lon = pwords[1].split("&")[0]
+            code = pwords[1].split("&")[1].split("=")[1]
             text = ""
             for a in range(1, len(words)):
                 text = text + words[a] + " "
                 
                     
-            return lat, lon, code, text
+        return lat, lon, code, text
                 
     # -------------------------------------------------------------------------
     # Send SMS
