bc3bbfd3ed49c8d71377cde1757f46f512aa1235
Tamara Atanasoska <tamara.atanasoska20@gmail.com>
2012-08-20 09:54:22 +0200

    One line fix.

--- src/apps/ecidadania/voting/views.py
+++ src/apps/ecidadania/voting/views.py
@@ -261,7 +261,7 @@ class EditVoting(UpdateView):
     :context: get_place
     """
     model = Voting
-    template_name = 'voting/voting_editform.html'
+    template_name = 'voting/voting_form.html'
 
     def get_success_url(self):
         self.space = get_object_or_404(Space, url=self.kwargs['space_url'])
