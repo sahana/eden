e4eb2979d590ec1c5e37c05e80ef590b8d53044b
Oscar Carballal Prego <info@oscarcp.com>
2012-09-05 20:44:47 +0200

    Replaced old monolithic view by the new modular ones

--- src/core/spaces/urls.py
+++ src/core/spaces/urls.py
@@ -25,97 +25,114 @@ from django.conf.urls import *
 from django.utils.translation import ugettext_lazy as _
 
 from core.spaces.views.spaces import GoToSpace, ViewSpaceIndex, ListSpaces, \
-                                    DeleteSpace
+                                    DeleteSpace, EditRole
 from core.spaces.views.documents import ListDocs, DeleteDocument, \
                                     AddDocument, EditDocument
 from core.spaces.views.events import ListEvents, DeleteEvent, ViewEvent, \
                                     AddEvent, EditEvent
-                                    
-from core.spaces.views import GoToSpace, ViewSpaceIndex, ListSpaces,\
-                            DeleteSpace, ListDocs, DeleteDocument, \
-                            ListEvents, DeleteEvent, ViewEvent, \
-                            ListPosts, SpaceFeed, AddDocument, \
-                            EditDocument, AddEvent, EditEvent, \
-                            ValidateIntent, EditRole
+from core.spaces.views.rss import SpaceFeed
+from core.spaces.views.intent import ValidateIntent
+from core.spaces.views.news import ListPosts
 
 # NOTICE: Don't change the order of urlpatterns or it will probably break.
 
-urlpatterns = patterns('core.spaces.views',
+urlpatterns = patterns('',
 
     # RSS Feed
     url(_(r'^(?P<space_url>\w+)/rss/$'), SpaceFeed(), name='space-feed'),
     
     # News
-    url(_(r'^(?P<space_url>\w+)/news/'), include('apps.ecidadania.news.urls')),
+    url(_(r'^(?P<space_url>\w+)/news/'),
+        include('apps.ecidadania.news.urls')),
 
     # Proposals
-    url(_(r'^(?P<space_url>\w+)/proposal/'), include('apps.ecidadania.proposals.urls')),
+    url(_(r'^(?P<space_url>\w+)/proposal/'),
+        include('apps.ecidadania.proposals.urls')),
     
     # Calendar
-    url(_(r'^(?P<space_url>\w+)/calendar/'), include('apps.ecidadania.cal.urls')),
+    url(_(r'^(?P<space_url>\w+)/calendar/'),
+        include('apps.ecidadania.cal.urls')),
 
     # Debates
-    url(_(r'^(?P<space_url>\w+)/debate/'), include('apps.ecidadania.debate.urls')),
+    url(_(r'^(?P<space_url>\w+)/debate/'),
+        include('apps.ecidadania.debate.urls')),
     
     # Debates
-    url(_(r'^(?P<space_url>\w+)/voting/'), include('apps.ecidadania.voting.urls')),
+    url(_(r'^(?P<space_url>\w+)/voting/'),
+        include('apps.ecidadania.voting.urls')),
 
 )
 
 # Document URLs
 urlpatterns += patterns('',
 
-    url(_(r'^(?P<space_url>\w+)/docs/add/$'), AddDocument.as_view(), name='add-document'),
+    url(_(r'^(?P<space_url>\w+)/docs/add/$'), AddDocument.as_view(),
+        name='add-document'),
 
-    url(_(r'^(?P<space_url>\w+)/docs/(?P<doc_id>\d+)/edit/$'), EditDocument.as_view(), name='edit-document'),
+    url(_(r'^(?P<space_url>\w+)/docs/(?P<doc_id>\d+)/edit/$'),
+        EditDocument.as_view(), name='edit-document'),
 
-    url(_(r'^(?P<space_url>\w+)/docs/(?P<doc_id>\d+)/delete/$'), DeleteDocument.as_view(), name='delete-document'),
+    url(_(r'^(?P<space_url>\w+)/docs/(?P<doc_id>\d+)/delete/$'),
+        DeleteDocument.as_view(), name='delete-document'),
 
-    url(_(r'^(?P<space_url>\w+)/docs/$'), ListDocs.as_view(), name='list-documents'),
+    url(_(r'^(?P<space_url>\w+)/docs/$'), ListDocs.as_view(),
+        name='list-documents'),
 
 )
 
 # Event URLs
 urlpatterns += patterns('',
 
-    url(_(r'^(?P<space_url>\w+)/event/add/$'), AddEvent.as_view(), name='add-event'),
+    url(_(r'^(?P<space_url>\w+)/event/add/$'), AddEvent.as_view(),
+        name='add-event'),
 
-    url(_(r'^(?P<space_url>\w+)/event/(?P<event_id>\d+)/edit/$'), EditEvent.as_view(), name='edit-event'),
+    url(_(r'^(?P<space_url>\w+)/event/(?P<event_id>\d+)/edit/$'),
+        EditEvent.as_view(), name='edit-event'),
 
-    url(_(r'^(?P<space_url>\w+)/event/(?P<event_id>\d+)/delete/$'), DeleteEvent.as_view(), name='delete-event'),
+    url(_(r'^(?P<space_url>\w+)/event/(?P<event_id>\d+)/delete/$'),
+        DeleteEvent.as_view(), name='delete-event'),
 
-    url(_(r'^(?P<space_url>\w+)/event/(?P<event_id>\d+)/$'), ViewEvent.as_view(), name='view-event'),
+    url(_(r'^(?P<space_url>\w+)/event/(?P<event_id>\d+)/$'),
+        ViewEvent.as_view(), name='view-event'),
 
-    url(_(r'^(?P<space_url>\w+)/event/$'), ListEvents.as_view(), name='list-events'),
+    url(_(r'^(?P<space_url>\w+)/event/$'), ListEvents.as_view(),
+        name='list-events'),
 
 )
 
 # Intent URLs
-urlpatterns += patterns('core.spaces.views',
+urlpatterns += patterns('',
 
-    url(_(r'^(?P<space_url>\w+)/intent/$'), 'add_intent', name='add-intent'),
+    url(_(r'^(?P<space_url>\w+)/intent/$'),
+        'core.spaces.views.intent.add_intent', name='add-intent'),
     url(_(r'^(?P<space_url>\w+)/intent/approve/(?P<token>\w+)/$'), ValidateIntent.as_view(), name='validate-intent'),
 
 )
 
 # Spaces URLs
-urlpatterns += patterns('core.spaces.views',
+urlpatterns += patterns('',
 
-    url(_(r'^(?P<space_url>\w+)/edit/'), 'edit_space', name='edit-space'),
+    url(_(r'^(?P<space_url>\w+)/edit/'),
+        'core.spaces.views.spaces.edit_space', name='edit-space'),
 
-    url(_(r'^(?P<space_url>\w+)/delete/'), DeleteSpace.as_view(), name='delete-space'),
+    url(_(r'^(?P<space_url>\w+)/delete/'), DeleteSpace.as_view(),
+        name='delete-space'),
     
-    url(_(r'^(?P<space_url>\w+)/news/'), ListPosts.as_view(), name='list-space-news'),
+    url(_(r'^(?P<space_url>\w+)/news/'), ListPosts.as_view(),
+        name='list-space-news'),
         
-    url(_(r'^add/$'), 'create_space', name='create-space'),
+    url(_(r'^add/$'), 'core.spaces.views.spaces.create_space',
+        name='create-space'),
 
     url(r'^$', ListSpaces.as_view(), name='list-spaces'),
 
     url(_(r'^go/'), GoToSpace.as_view(), name='goto-space'),
 
-    url(_(r'^(?P<space_url>\w+)/roles/'), EditRole.as_view(), name='edit-roles'),
+    url(_(r'^(?P<space_url>\w+)/roles/'), EditRole.as_view(),
+        name='edit-roles'),
 
-    url(r'^(?P<space_url>\w+)/$', ViewSpaceIndex.as_view(), name='space-index'),
+    url(r'^(?P<space_url>\w+)/$', ViewSpaceIndex.as_view(),
+        name='space-index'),
 
 )
 
---
+++ src/core/spaces/views/news.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2010-2012 Cidadania S. Coop. Galega
+#
+# This file is part of e-cidadania.
+#
+# e-cidadania is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# e-cidadania is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with e-cidadania. If not, see <http://www.gnu.org/licenses/>.
+
+from django.views.generic.list import ListView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404
+from django.utils.translation import ugettext_lazy as _
+
+from core.spaces.models import Space
+from apps.news.models import Post
+from core.permissions import has_space_permission, has_all_permissions
+
+class ListPosts(ListView):
+
+    """
+    Returns a list with all the posts attached to that space. It's similar to
+    an archive, but without classification or filtering.
+    
+    :rtype: Object list
+    :context: post_list
+    """
+    paginate_by = 10
+    context_object_name = 'post_list'
+    template_name = 'news/news_list.html'
+    
+    def get_queryset(self):
+        place = get_object_or_404(Space, url=self.kwargs['space_url'])
+        return Post.objects.all().filter(space=place).order_by('-pub_date')
+    
+    def get_context_data(self, **kwargs):
+        context = super(ListPosts, self).get_context_data(**kwargs)
+        context['get_place'] = get_object_or_404(Space,
+        	url=self.kwargs['space_url'])
+        return context
