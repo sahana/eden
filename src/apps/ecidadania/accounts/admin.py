d81c4f0cc33a402cbc601a8f13a320a00f05ad37
Oscar Carballal Prego <info@oscarcp.com>
2012-09-05 09:35:27 +0200

    Fixed #194: Mass mailing AdminAction

--- src/apps/ecidadania/accounts/admin.py
+++ src/apps/ecidadania/accounts/admin.py
@@ -20,8 +20,10 @@
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.contrib.auth.models import User
+from django.core.mail import send_mail, send_mass_mail
+from django.template import RequestContext
 
 from apps.ecidadania.accounts.models import UserProfile
 
@@ -41,15 +43,16 @@ class ProfileAdmin(admin.ModelAdmin):
         """
         #selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
         # ct = ContentType.objects.get_for_model(queryset.model)
-        if request.method == "POST":
+        if 'sendmail' in request.POST:
             for obj in queryset:
                 get_user = get_object_or_404(User, id=obj.id)
-                send.mail(request.POST['massmail_subject'], request.POST['message'], 'noreply@ecidadania.org', [get_user.email])
+                send_mail(request.POST['massmail_subject'], request.POST['message'], 'noreply@ecidadania.org', [get_user.email])
             return HttpResponseRedirect(request.get_full_path())
         
         selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
         ct = ContentType.objects.get_for_model(queryset.model)
-        return render_to_response('/mail/massmail.html', { 'people': selected })
+        return render_to_response('mail/massmail.html', { 'people': selected },
+                                context_instance=RequestContext(request))
     mass_mail.short_description = 'Send a global mail to the selected users'
     
 admin.site.register(UserProfile, ProfileAdmin)
