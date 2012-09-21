4fff99c94f3535d988abdc565cd1a113a53f129e
Kunal Hari <kunal@aidiq.com>
2012-09-21 13:52:43 +1200

    Test framework work

--- modules/tests/inv/receive_item.py
+++ modules/tests/inv/receive_item.py
@@ -59,8 +59,14 @@ class ReceiveItem(InvTestFunctions):
                       "Same Warehouse (Warehouse)",
                       "option",
                      ),
+                    ("type",
+                      "Internal Shipment",
                      ("type",
                       "-",
+                      "option",
+                     ),
+                    ("from_site_id",
+                      "Lori (Facility)",
                       "option",
                      )
                     ]
