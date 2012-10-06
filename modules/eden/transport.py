b7c9147513ef95c4e715c24fad0122446b8672a8
Lauren Ryan <lxryan@live.com>
2012-10-06 15:24:04 -0400

    added fields for seaport
    GHC

--- modules/eden/transport.py
+++ modules/eden/transport.py
@@ -48,7 +48,8 @@ class S3TransportModel(S3Model):
     def model(self):
 
         T = current.T
-
+        messages = current.messages
+        UNKNOWN_OPT = messages.UNKNOWN_OPT
         configure = self.configure
         crud_strings = current.response.s3.crud_strings
         define_table = self.define_table
@@ -110,6 +111,29 @@ class S3TransportModel(S3Model):
         # ---------------------------------------------------------------------
         # Seaports
         #
+        
+        storageTyp_opts = {
+            1:"",
+            2:T("warehousing"),
+            3:T("secure storage"),
+            4:T("customs warehousing")
+        }
+        
+        operationStatus_opts = {
+            1: T("Operational"),
+            2: T("Closed")
+        }
+        
+        ownership_opts = {
+            1: T("Public"),
+            2: T("Private")
+        }
+        
+        heightUnit_opts = {
+            1: T("ft"),
+            2: T("m")
+        }
+        
         tablename = "transport_seaport"
         table = define_table(tablename,
                              self.super_link("site_id", "org_site"),
@@ -126,6 +150,112 @@ class S3TransportModel(S3Model):
                                    #notnull=True,
                                    #unique=True,
                                    label=T("Code")),
+                             Field("ownershipType", "integer",
+                                   requires = IS_IN_SET(ownership_opts, zero=None),
+                                   default = 1,
+                                   label = T("Ownership"),
+                                   represent = lambda opt: \
+                                   ownership_opts.get(opt, UNKNOWN_OPT)),
+                             Field("operationStatus", "integer",
+                                   requires = IS_IN_SET(operationStatus_opts, zero=None),
+                                   default = 1,
+                                   label = T("Operation Status"),
+                                   represent = lambda opt: \
+                                   operationStatus_opts.get(opt, UNKNOWN_OPT)),
+                             Field("maxHeight", "double",
+                                   label=T("Max Height")),
+                             Field("maxHeightUnits", "integer",
+                                   requires = IS_IN_SET(heightUnit_opts, zero=None),
+                                   default = 1,
+                                   label = T("Units"),
+                                   represent = lambda opt: \
+                                   heightUnit_opts.get(opt, UNKNOWN_OPT)),
+                             
+                             Field("roll_OnOff", "boolean",
+                                   default=False,
+                                   label=T("Roll On Roll Off Berth")),
+                                    
+                             Field("cargoPierDepth", "double",
+                                   label=T("Cargo Pier Depth")),                             
+                             Field("cargoPierDepthUnits", "integer",
+                                   requires = IS_IN_SET(heightUnit_opts, zero=None),
+                                   default = 1,
+                                   label = T("Units"),
+                                   represent = lambda opt: \
+                                   heightUnit_opts.get(opt, UNKNOWN_OPT)),
+                             
+                             Field("oilTerminalDepth", "double",
+                                   label=T("Oil Terminal Depth")),
+                             Field("oilTerminalDepthUnits", "integer",
+                                   requires = IS_IN_SET(heightUnit_opts, zero=None),
+                                   default = 1,
+                                   label = T("Units"),
+                                   represent = lambda opt: \
+                                   heightUnit_opts.get(opt, UNKNOWN_OPT)),
+                             
+                             Field("dryDock", "boolean",
+                                    default=False,
+                                    label=T("Dry Dock")),
+                                    
+                             Field("vesselMaxLength", "double",
+                                   label=T("Vessel Max Length")),
+                             Field("vesselMaxLengthUnits", "integer",
+                                   requires = IS_IN_SET(heightUnit_opts, zero=None),
+                                   default = 1,
+                                   label = T("Units"),
+                                   represent = lambda opt: \
+                                   heightUnit_opts.get(opt, UNKNOWN_OPT)),
+                             Field("seaportRepairs","text",
+                                   label=T("Seaport Repairs")),
+                             Field ("seaportShelter","text",
+                                   label=T("Seaport Shelter")),
+                             Field("wareCap", "double",     
+                                   label=T("Warehousing Storage Capacity")),
+                             Field("secStrCap", "double",      
+                                   label=T("Secure Storage Capacity")),
+                             Field("custWareCap", "double",     
+                                   label=T("Customs Warehousing Storage Capacity")),
+                             Field("tugNum", "integer",    
+                                   label=T("Number of Tugboats")),
+                             Field("tugCap", "double",     
+                                   label=T("Tugboat Capacity")),
+                             Field("bargeNum", "integer",     
+                                   label=T("Number of Barges")),
+                             Field("bargeCap", "double",     
+                                   label=T("Barge Capacity")),
+                             Field("seaportLoadingEquipment","text",
+                                   label=T("Seaport Loading Equipment")),
+                             Field("seaportCustomsCapacity","text",
+                                   label=T("Seaport Customs Capacity")),
+                             Field ("seaportSecurity","text",
+                                   label=T("Seaport Security")),
+                             Field("highTideDepth", "double",
+                                   label=T("High Tide Depth")),                             
+                             Field("highTideDepthUnits", "integer",
+                                   requires = IS_IN_SET(heightUnit_opts, zero=None),
+                                   default = 1,
+                                   label = T("Units"),
+                                   represent = lambda opt: \
+                                   heightUnit_opts.get(opt, UNKNOWN_OPT)),
+                             
+                             Field("lowTideDepth", "double",
+                                   label=T("Low Tide Depth")),
+                             Field("lowTideDepthUnits", "integer",
+                                   requires = IS_IN_SET(heightUnit_opts, zero=None),
+                                   default = 1,
+                                   label = T("Units"),
+                                   represent = lambda opt: \
+                                   heightUnit_opts.get(opt, UNKNOWN_OPT)),
+                             
+                             Field("floodDepth", "double",
+                                   label=T("Flood Depth")),                             
+                             Field("floodDepthUnits", "integer",
+                                   requires = IS_IN_SET(heightUnit_opts, zero=None),
+                                   default = 1,
+                                   label = T("Units"),
+                                   represent = lambda opt: \
+                                   heightUnit_opts.get(opt, UNKNOWN_OPT)),
+                             
                              self.org_organisation_id(widget=S3OrganisationAutocompleteWidget(
                                 default_from_profile=True)),
                              self.gis_location_id(),
