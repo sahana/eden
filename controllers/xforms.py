4b01f9699a33a91538c6b23ed72b90671d172510
Fran Boon <fran@aidiq.com>
2012-10-11 09:11:02 +0100

    Some XForms fixes

--- controllers/xforms.py
+++ controllers/xforms.py
@@ -6,37 +6,40 @@
 
 module = request.controller
 
+# -----------------------------------------------------------------------------
 def create():
     """
-    Given a Table, returns an XForms to create an instance:
-    http://code.javarosa.org/wiki/buildxforms
-    http://www.w3schools.com/xforms/
-    http://oreilly.com/catalog/9780596003692/preview.html
-    Known field requirements that don't work properly:
-    IS_IN_DB
-    IS_NOT_ONE_OF
-    IS_EMAIL
-    IS_DATE_IN_RANGE
-    IS_DATETIME_IN_RANGE
+        Given a Table, returns an XForms to create an instance:
+        http://code.javarosa.org/wiki/buildxforms
+        http://www.w3schools.com/xforms/
+        http://oreilly.com/catalog/9780596003692/preview.html
+        Known field requirements that don't work properly:
+        IS_IN_DB
+        IS_NOT_ONE_OF
+        IS_EMAIL
+        IS_DATE_IN_RANGE
+        IS_DATETIME_IN_RANGE
     """
-    if len(request.args) == 0:
+
+    try:
+        tablename = request.args[0]
+    except:
         session.error = T("Need to specify a table!")
         redirect(URL(r=request))
-    _table = request.args[0]
 
-    title = _table
-    table = db[_table]
+    title = tablename
+    table = s3db[tablename]
 
     instance_list = []
     bindings_list = []
     controllers_list = []
-    itext_list = [TAG["text"](TAG["value"](s3.crud_strings[_table].title_list), _id="title"),]
+    itext_list = [TAG["text"](TAG["value"](s3.crud_strings[tablename].title_list), _id="title"),]
 
     for field in table.fields:
         if field in ["id", "created_on", "modified_on", "uuid", "mci", "deleted",
                      "created_by", "modified_by", "deleted_fk", "owned_by_group",
                      "owned_by_user"]:
-            # This will get added server-side
+            # These will get added server-side
             pass
         elif table[field].writable == False and table[field].readable == False:
             pass
@@ -56,13 +59,16 @@ def create():
     response.headers["Content-Type"] = "text/xml"
     response.view = "xforms.xml"
 
-    return dict(title=title, instance=instance, bindings=bindings, controllers=controllers, itext_list=itext_list)
+    return dict(title=title, instance=instance, bindings=bindings,
+                controllers=controllers, itext_list=itext_list)
 
+# -----------------------------------------------------------------------------
 def uses_requirement(requirement, field):
     """
-    Check if a given database field uses the specified requirement
-    (IS_IN_SET, IS_INT_IN_RANGE, etc)
+        Check if a given database field uses the specified requirement
+        (IS_IN_SET, IS_INT_IN_RANGE, etc)
     """
+
     if hasattr(field.requires, "other") or requirement in str(field.requires):
         if hasattr(field.requires, "other"):
             if requirement in str(field.requires.other):
@@ -71,9 +77,10 @@ def uses_requirement(requirement, field):
             return True
     return False
 
+# -----------------------------------------------------------------------------
 def generate_instance(table, field):
     """
-    Generates XML for the instance of the specified field.
+        Generates XML for the instance of the specified field.
     """
 
     if table[field].default:
@@ -83,9 +90,10 @@ def generate_instance(table, field):
 
     return instance
 
+# -----------------------------------------------------------------------------
 def generate_bindings(table, field, ref):
     """
-    Generates the XML for bindings for the specified database field.
+        Generates the XML for bindings for the specified database field.
     """
 
     if "IS_NOT_EMPTY" in str(table[field].requires):
@@ -129,58 +137,75 @@ def generate_bindings(table, field, ref):
             constraint = "(. > " + str(minimum) + ")"
         else:
             constraint = "(. > " + str(minimum) + " and . < " + str(maximum) + ")"
-        binding = TAG["bind"](_nodeset=ref, _type=_type, _required=required, _constraint=constraint)
-#   elif uses_requirement("IS_DATETIME_IN_RANGE", field):
-#      pass
-#   elif uses_requirement("IS_EMAIL", field):
-#      pass
+        binding = TAG["bind"](_nodeset=ref,
+                              _type=_type,
+                              _required=required,
+                              _constraint=constraint)
+
+    #elif uses_requirement("IS_DATETIME_IN_RANGE", field):
+    #   pass
+
+    #elif uses_requirement("IS_EMAIL", field):
+    #   pass
+
     elif uses_requirement("IS_IN_SET", table[field]):
         binding = TAG["bind"](_nodeset=ref, _required=required)
+
     else:
         binding = TAG["bind"](_nodeset=ref, _type=_type, _required=required)
 
     return binding
 
+# -----------------------------------------------------------------------------
 def generate_controllers(table, field, ref):
     """
-    Generates the controllers XML for the database table field.
+        Generates the controllers XML for the database table field.
     """
+
     itext_list = [] # Internationalization
     controllers_list = []
 
-    itext_list.append(TAG["text"](TAG["value"](table[field].label), _id=ref + ":label"))
-    itext_list.append(TAG["text"](TAG["value"](table[field].comment), _id=ref + ":hint"))
+    itext_list.append(TAG["text"](TAG["value"](table[field].label),
+                                  _id=ref + ":label"))
+    itext_list.append(TAG["text"](TAG["value"](table[field].comment),
+                                  _id=ref + ":hint"))
 
     if hasattr(table[field].requires, "option"):
         items_list = []
         for option in table[field].requires.theset:
             items_list.append(TAG["item"](TAG["label"](option), TAG["value"](option)))
         controllers_list.append(TAG["select1"](items_list, _ref=field))
+
     #elif uses_requirement("IS_IN_DB", field):
         # ToDo (similar to IS_IN_SET)?
         #pass
+
     #elif uses_requirement("IS_NOT_ONE_OF", field):
         # ToDo
         #pass
+
     elif uses_requirement("IS_IN_SET", table[field]): # Defined below
         if hasattr(table[field].requires, "other"):
             insetrequires = table[field].requires.other
         else:
             insetrequires = table[field].requires
         theset = insetrequires.theset
-        items_list=[]
+        items_list = []
         items_list.append(TAG["label"](_ref="jr:itext('" + ref + ":label')"))
         items_list.append(TAG["hint"](_ref="jr:itext('" + ref + ":hint')"))
 
-        option_num = 0 # for formatting something like "jr:itext('stuff:option0')"
-        for option in theset:
-            if table[field].type == "integer":
-                option = int(option)
-            option_ref = ref + ":option" + str(option_num)
-            items_list.append(TAG["item"](TAG["label"](_ref="jr:itext('" + option_ref + "')"), TAG["value"](option)))
-            #itext_list.append(TAG["text"](TAG["value"](table[field].represent(option)), _id=option_ref))
-            itext_list.append(TAG["text"](TAG["value"](insetrequires.labels[theset.index(str(option))]), _id=option_ref))
-            option_num += 1
+        if theset:
+            option_num = 0 # for formatting something like "jr:itext('stuff:option0')"
+            for option in theset:
+                if table[field].type == "integer":
+                    option = int(option)
+                option_ref = ref + ":option" + str(option_num)
+                items_list.append(TAG["item"](TAG["label"](_ref="jr:itext('" + option_ref + "')"),
+                                              TAG["value"](option)))
+                #itext_list.append(TAG["text"](TAG["value"](table[field].represent(option)), _id=option_ref))
+                itext_list.append(TAG["text"](TAG["value"](insetrequires.labels[theset.index(str(option))]),
+                                              _id=option_ref))
+                option_num += 1
         if insetrequires.multiple:
             controller = TAG["select"](items_list, _ref=ref)
         else:
@@ -192,31 +217,39 @@ def generate_controllers(table, field, ref):
         items_list.append(TAG["label"](_ref="jr:itext('" + ref + ":label')"))
         items_list.append(TAG["hint"](_ref="jr:itext('" + ref + ":hint')"))
         # True option
-        items_list.append(TAG["item"](TAG["label"](_ref="jr:itext('" + ref + ":option0')"), TAG["value"](1)))
-        itext_list.append(TAG["text"](TAG["value"]("True"), _id=ref + ":option0"))
+        items_list.append(TAG["item"](TAG["label"](_ref="jr:itext('" + ref + ":option0')"),
+                                      TAG["value"](1)))
+        itext_list.append(TAG["text"](TAG["value"]("True"),
+                                      _id=ref + ":option0"))
         # False option
-        items_list.append(TAG["item"](TAG["label"](_ref="jr:itext('" + ref + ":option1')"), TAG["value"](0)))
-        itext_list.append(TAG["text"](TAG["value"]("False"), _id=ref + ":option1"))
+        items_list.append(TAG["item"](TAG["label"](_ref="jr:itext('" + ref + ":option1')"),
+                                      TAG["value"](0)))
+        itext_list.append(TAG["text"](TAG["value"]("False"),
+                                      _id=ref + ":option1"))
 
         controller = TAG["select1"](items_list, _ref=ref)
 
     elif table[field].type == "upload": # For uploading images
         items_list=[]
-
         items_list.append(TAG["label"](_ref="jr:itext('" + ref + ":label')"))
         items_list.append(TAG["hint"](_ref="jr:itext('" + ref + ":hint')"))
         controller = TAG["upload"](items_list, _ref=ref, _mediatype="image/*")
+
     elif table[field].writable == False:
-        controller = TAG["input"](TAG["label"](table[field].label), _ref=ref, _readonly="true", _default=table[field].default.upper())
+        controller = TAG["input"](TAG["label"](table[field].label), _ref=ref,
+                                  _readonly="true",
+                                  _default=table[field].default.upper())
+
     else:
         # Normal Input field
         controller = TAG["input"](TAG["label"](table[field].label), _ref=ref)
 
     return controller, itext_list
 
+# -----------------------------------------------------------------------------
 def csvdata(nodelist):
     """
-    Returns the data in the given node as a comma separated string
+        Returns the data in the given node as a comma separated string
     """
 
     data = ""
@@ -228,9 +261,10 @@ def csvdata(nodelist):
                 data = data+ ","
     return data[1:] + "\n"
 
+# -----------------------------------------------------------------------------
 def csvheader(parent, nodelist):
     """
-    Gives the header for the CSV
+        Gives the header for the CSV
     """
 
     header = ""
@@ -239,10 +273,11 @@ def csvheader(parent, nodelist):
             header = header + "," + parent + "." + subnode.tagName
     return header[1:] + "\n"
 
+# -----------------------------------------------------------------------------
 def importxml(db, xmlinput):
     """
-    Converts the XML to a CSV compatible with the import_from_csv_file of web2py
-    ToDo: rewrite this to go via S3XRC for proper Authz checking, Audit, Create/Update checking.
+        Converts the XML to a CSV compatible with the import_from_csv_file of web2py
+        @ToDo: rewrite this to go via S3Resource for proper Auth checking, Audit.
     """
 
     import cStringIO
@@ -262,31 +297,41 @@ def importxml(db, xmlinput):
     fh.seek(0, 0)
     db[parent].import_from_csv_file(fh)
 
+# -----------------------------------------------------------------------------
 @auth.s3_requires_membership(1)
 def post():
     data = importxml(db, request.body.read())
     return data
 
-@auth.s3_requires_membership(2)
-def submission_old():
+# -----------------------------------------------------------------------------
+def formList():
     """
-    Allows for submission of xforms by ODK Collect
-    http://code.google.com/p/opendatakit/
+        Generates a list of Xforms based on database tables for ODK Collect
+        http://code.google.com/p/opendatakit/
     """
-    response.headers["Content-Type"] = "text/xml"
-    xml = str(request.post_vars.xml_submission_file.value)
-    if len(xml) == 0:
-        raise HTTP(400, "Need some xml!")
-    importxml(db, xml)
-    r = HTTP(201, "Saved.")
-    r.headers["Location"] = request.env.http_host
-    raise r
 
+    # Test statements
+    #xml = TAG.forms(*[TAG.form(getName("Name"), _url = "http://" + request.env.http_host + URL(c="static", "current.xml"))])
+    #xml = TAG.forms(*[TAG.form(getName(t), _url = "http://" + request.env.http_host + URL(f="create", args=t)) for t in db.tables()])
+
+    # List of a couple simple tables to avoid a giant list of all the tables
+    #tables = ["pf_missing_report", "pr_presence"]
+    tables = ["irs_ireport", "rms_req", "cr_shelter", "pr_person", "pr_image"]
+    xml = TAG.forms()
+    for tablename in tables:
+        xml.append(TAG.form(get_name(tablename),
+                            _url = "http://" + request.env.http_host + URL(f="create", args=tablename)))
+
+    response.headers["Content-Type"] = "text/xml"
+    response.view = "xforms.xml"
+    return xml
+
+# -----------------------------------------------------------------------------
 @auth.s3_requires_membership(2)
 def submission():
     """
-    Allows for submission of Xforms by ODK Collect
-    using the S3XRC framework.
+        Allows for submission of Xforms by ODK Collect
+        http://code.google.com/p/opendatakit/
     """
 
     try:
@@ -309,15 +354,14 @@ def submission():
         raise HTTP(400, "Invalid Request: Expected an XForm")
 
     tree = etree.parse(xmlinput)
-    resource = tree.getroot().tag
+    tablename = tree.getroot().tag
 
-    prefix, name = resource.split("_")
-    res = current.s3db.resource(resource)
+    resource = s3db.resource(tablename)
 
     stylesheet = os.path.join(request.folder, "static", "formats", "odk", "import.xsl")
 
     try:
-        result = res.import_xml(source=tree, stylesheet=stylesheet)
+        result = resource.import_xml(source=tree, stylesheet=stylesheet)
     except IOError, SyntaxError:
         raise HTTP(500, "Internal server error")
 
@@ -331,30 +375,28 @@ def submission():
     else:
         raise HTTP(status, result)
 
-def formList():
+# -----------------------------------------------------------------------------
+@auth.s3_requires_membership(2)
+def submission_old():
     """
-    Generates a list of Xforms based on database tables for ODK Collect
-    http://code.google.com/p/opendatakit/
-    ToDo: Provide a static list of forms, such as RMS_Req, GIS_Landmark, MPR_Missing_Report, CR_Shelter, PR_Presence
+        Allows for submission of xforms by ODK Collect
+        http://code.google.com/p/opendatakit/
     """
-    # Test statements
-    #xml = TAG.forms(*[TAG.form(getName("Name"), _url = "http://" + request.env.http_host + URL(c="static", "current.xml"))])
-    #xml = TAG.forms(*[TAG.form(getName(t), _url = "http://" + request.env.http_host + URL(f="create", args=t)) for t in db.tables()])
-
-    # List of a couple simple tables to avoid a giant list of all the tables
-    tables = ["rms_req", "pf_missing_report", "cr_shelter", "pr_presence", "pr_person", "pr_image"]
-    xml = TAG.forms()
-    for table in tables:
-        xml.append(TAG.form(get_name(table), _url = "http://" + request.env.http_host + URL(f="create", args=db[table])))
-
     response.headers["Content-Type"] = "text/xml"
-    response.view = "xforms.xml"
-    return xml
+    xml = str(request.post_vars.xml_submission_file.value)
+    if len(xml) == 0:
+        raise HTTP(400, "Need some xml!")
+    importxml(db, xml)
+    r = HTTP(201, "Saved.")
+    r.headers["Location"] = request.env.http_host
+    raise r
 
-def get_name(name):
+# -----------------------------------------------------------------------------
+def get_name(tablename):
     """
         Generates a pretty(er) name from a database table name.
     """
-    return name[name.find("_") + 1:].replace("_", " ").capitalize()
 
+    return tablename[tablename.find("_") + 1:].replace("_", " ").capitalize()
 
+# END =========================================================================
