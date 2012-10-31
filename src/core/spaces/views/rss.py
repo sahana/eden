f583dbccedc5bbf47c7453466a1c18e61358b5f3
Oscar Carballal Prego <info@oscarcp.com>
2012-10-31 12:32:23 +0100

    Fixed #210 Authentication in RSS feeds

--- src/core/spaces/views/rss.py
+++ src/core/spaces/views/rss.py
@@ -18,10 +18,13 @@
 # along with e-cidadania. If not, see <http://www.gnu.org/licenses/>.
 
 import itertools
+import base64
 
 from django.contrib.syndication.views import Feed
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 from apps.ecidadania.proposals.models import Proposal
 from apps.ecidadania.debate.models import Debate
@@ -29,7 +32,38 @@ from apps.ecidadania.news.models import Post
 from core.spaces.models import Space, Event
 
 
-class SpaceFeed(Feed):
+class HTTPAuthFeed(Feed):
+    basic_auth_realm = 'e-cidadania'
+    
+    def __call__(self, request, *args, **kwargs):
+        # HTTP auth check inspired by http://djangosnippets.org/snippets/243/
+        if request.user.is_authenticated():
+            # already logged in
+            return super(HTTPAuthFeed, self).__call__(request, *args, **kwargs)
+    
+        # check HTTP auth credentials
+        if 'HTTP_AUTHORIZATION' in request.META:
+            auth = request.META['HTTP_AUTHORIZATION'].split()
+            if len(auth) == 2:
+                # only basic auth is supported
+                if auth[0].lower() == "basic":
+                    uname, passwd = base64.b64decode(auth[1]).split(':')
+                    user = authenticate(username=uname, password=passwd)
+                    if user is not None:
+                        if user.is_active:
+                            login(request, user)
+                            request.user = user
+                            return super(HTTPAuthFeed, self).__call__(request,
+                                *args, **kwargs)
+        
+        # missing auth header or failed authentication results in 401
+        response = HttpResponse()
+        response.status_code = 401
+        response['WWW-Authenticate'] = 'Basic realm="%s"' % self.basic_auth_realm
+        return response
+
+
+class SpaceFeed(HTTPAuthFeed):
 
     """
     Returns a space feed with the content of various applications. In the
