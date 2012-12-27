46fbcdfd7bcff4b5bf9cd7c47a63c4bd8171c33e
Dominic König <dominic@nursix.org>
2012-12-27 21:07:35 +0100

    Fix #1362

--- modules/s3/s3forms.py
+++ modules/s3/s3forms.py
@@ -203,7 +203,7 @@ class S3SQLDefaultForm(S3SQLForm):
                                           format=format)
 
             # De-duplicate link table entries
-            record_id = self.deduplicate_link(request, record_id)
+            self.record_id = record_id = self.deduplicate_link(request, record_id)
 
             # Add asterisk to labels of required fields
             mark_required = self._config("mark_required", default = [])
@@ -391,6 +391,7 @@ class S3SQLDefaultForm(S3SQLForm):
                 query = (table[lkey] == _lkey) & (table[rkey] == _rkey)
                 row = current.db(query).select(table._id, limitby=(0, 1)).first()
                 if row is not None:
+                    tablename = self.tablename
                     record_id = row[pkey]
                     formkey = session.get("_formkey[%s/None]" % tablename)
                     formname = "%s/%s" % (tablename, record_id)
@@ -434,8 +435,7 @@ class S3SQLDefaultForm(S3SQLForm):
         success = True
         error = None
 
-        formname = "%s/%s" % (self.tablename,
-                              self.record_id)
+        formname = "%s/%s" % (self.tablename, self.record_id)
         if form.accepts(vars,
                         current.session,
                         formname=formname,
