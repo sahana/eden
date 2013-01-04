58ef1e4e725c8706aa94c2ff66d0d47c6df61112
Liezl P <liezl.200@gmail.com>
2013-01-04 16:49:23 -0500

    Fix#1352 - Preserve querystring when switching language

--- static/scripts/S3/S3.js
+++ static/scripts/S3/S3.js
@@ -1111,3 +1111,37 @@ S3.slider = function(fieldname, minval, maxval, steprange, value) {
 };
 
 // ============================================================================
+/**
+ * Reusable function to add a querystring variable to an existing URL and redirect into it.
+ * It accounts for existing variables and will override an existing one.
+ * Sample usage: _onchange="S3reloadWithQueryStringVars({'_language': $(this).val()});")
+ */
+
+function S3reloadWithQueryStringVars (queryStringVars) {
+    var existingQueryVars = location.search ? location.search.substring(1).split("&") : [],
+        currentUrl = location.search ? location.href.replace(location.search,"") : location.href,
+        newQueryVars = {},
+        newUrl = currentUrl + "?";
+    if(existingQueryVars.length > 0) {
+        for (var i = 0; i < existingQueryVars.length; i++) {
+            var pair = existingQueryVars[i].split("=");
+            newQueryVars[pair[0]] = pair[1];
+        }
+    }
+    if(queryStringVars) {
+        for (var queryStringVar in queryStringVars) {
+            newQueryVars[queryStringVar] = queryStringVars[queryStringVar];
+        }
+    }
+    if(newQueryVars) { 
+        for (var newQueryVar in newQueryVars) {
+            newUrl += newQueryVar + "=" + newQueryVars[newQueryVar] + "&";
+        }
+        newUrl = newUrl.substring(0, newUrl.length-1);
+        window.location.href = newUrl;
+    } else {
+        window.location.href = location.href;
+    }
+}
+
+// ============================================================================
