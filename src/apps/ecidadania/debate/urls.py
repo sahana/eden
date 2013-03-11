e4365b402c75da7176f032862953970b48ce997f
VishrutMehta <vishrut.mehta009@gmail.com>
2013-03-11 06:02:22 +0500

    [Enhancement]Ticket: #22 Created a Delete function for Debates

--- src/apps/ecidadania/debate/urls.py
+++ src/apps/ecidadania/debate/urls.py
@@ -24,7 +24,7 @@ access to '/spaces/'.
 from django.conf.urls import *
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.decorators import permission_required
-from apps.ecidadania.debate.views import ListDebates, ViewDebate, edit_debate 
+from apps.ecidadania.debate.views import ListDebates, ViewDebate, DeleteDebate, edit_debate
 from apps.ecidadania.debate.url_names import *
 
 urlpatterns = patterns('apps.ecidadania.debate.views',
@@ -46,6 +46,6 @@ urlpatterns = patterns('apps.ecidadania.debate.views',
     # Editing debates is not allowed at this time
     url(r'^edit/(?P<debate_id>\d+)/', 'edit_debate', name=DEBATE_EDIT),
 
-    #(r'^edit/(?P<pk>\d+)', 'delete_debate'),
+    url(r'^delete/(?P<debate_id>\d+)', DeleteDebate.as_view() ,name = DEBATE_DELETE),
 
 )
