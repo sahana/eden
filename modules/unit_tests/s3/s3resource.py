c008f009c2200630f8d7049b11698ec9f3aa074f
Dominic König <dominic@nursix.org>
2013-04-15 21:19:09 +0200

    Fix unit test for latest DAL interpretation of .contains()

--- modules/unit_tests/s3/s3resource.py
+++ modules/unit_tests/s3/s3resource.py
@@ -541,12 +541,20 @@ class ResourceFilterQueryTests(unittest.TestCase):
         FS = S3FieldSelector
         q = FS("multi_sector_id").contains([1, 2])
         query = q.query(resource)
-        self.assertEqual(str(query), "((org_organisation.multi_sector_id LIKE '%|1|%') AND "
-                                     "(org_organisation.multi_sector_id LIKE '%|2|%'))")
+        try:
+            self.assertEqual(str(query), "((org_organisation.multi_sector_id LIKE (CONCAT('%|',(REPLACE((REPLACE('1','%','%%')),'|','||')),'|%'))) AND "
+                                         "(org_organisation.multi_sector_id LIKE (CONCAT('%|',(REPLACE((REPLACE('2','%','%%')),'|','||')),'|%'))))")
+        except AssertionError:
+            self.assertEqual(str(query), "((org_organisation.multi_sector_id LIKE '%|1|%') AND "
+                                         "(org_organisation.multi_sector_id LIKE '%|2|%'))")
         q = FS("multi_sector_id").anyof([1, 2])
         query = q.query(resource)
-        self.assertEqual(str(query), "((org_organisation.multi_sector_id LIKE '%|1|%') OR "
-                                     "(org_organisation.multi_sector_id LIKE '%|2|%'))")
+        try:
+            self.assertEqual(str(query), "((org_organisation.multi_sector_id LIKE (CONCAT('%|',(REPLACE((REPLACE('1','%','%%')),'|','||')),'|%'))) OR "
+                                         "(org_organisation.multi_sector_id LIKE (CONCAT('%|',(REPLACE((REPLACE('2','%','%%')),'|','||')),'|%'))))")
+        except AssertionError:
+            self.assertEqual(str(query), "((org_organisation.multi_sector_id LIKE '%|1|%') OR "
+                                        "(org_organisation.multi_sector_id LIKE '%|2|%'))")
 
     # -------------------------------------------------------------------------
     def tearDown(self):
