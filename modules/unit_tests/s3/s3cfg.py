e5bb0b46a151ad2a187f2c20eb314547c2a024ed
Dominic König <dominic@nursix.org>
2013-07-03 10:00:24 +0200

    Migrate s3cfg unit test to fast_select.

--- modules/unit_tests/s3/s3cfg.py
+++ modules/unit_tests/s3/s3cfg.py
@@ -13,6 +13,7 @@ from gluon.dal import Query
 class S3ConfigTests(unittest.TestCase):
     """ Deployment settings tests """
 
+    # -------------------------------------------------------------------------
     def testSetOrgDependentFields(self):
         """ Test organisation-dependent accessibility of fields """
 
@@ -57,7 +58,7 @@ class S3ConfigTests(unittest.TestCase):
             msg = resource.import_xml(tree)
 
             resource = s3db.resource("org_organisation", uid="ExampleRootOrg")
-            org = resource.select()[0]
+            org = resource.fast_select(None, as_rows=True)[0]
             auth.override = False
 
             # Normal user can't see the field by default
