06c01c6fc5a8e37b0cadc17a74cbaae82b8bb67f
Michael Howden <michael@sahanafoundation.org>
2013-07-17 00:57:49 +1200

    LACRMT Changes * Organisations don't have coalitions * Add & fix Org Sector & Service imports * Add & Config Sample Org Report * Formatting of Inline Fields

--- private/templates/CRMT/config.py
+++ private/templates/CRMT/config.py
@@ -305,12 +305,6 @@ def customize_org_organisation(**attr):
         "name",
         "logo",
         S3SQLInlineComponentCheckbox(
-            "group",
-            label = T("Coalition"),
-            field = "group_id",
-            cols = 3,
-        ),
-        S3SQLInlineComponentCheckbox(
             "sector",
             label = T("Sectors"),
             field = "sector_id",
@@ -338,7 +332,7 @@ def customize_org_organisation(**attr):
             label = T("Organization's Locations"),
             #comment = "Bob",
             fields = ["name", 
-                      "facility_type_id",
+                      "site_facility_type.facility_type_id",
                       "location_id",
                       ],
         ),
@@ -354,12 +348,7 @@ def customize_org_organisation(**attr):
         "comments",
     ) 
 
-    filter_widgets = [S3OptionsFilter("group_membership.group_id",
-                                      label=T("Coalition"),
-                                      represent="%(name)s",
-                                      widget="multiselect",
-                                      ),
-                      S3OptionsFilter("sector_organisation.sector_id",
+    filter_widgets = [S3OptionsFilter("sector_organisation.sector_id",
                                       label=T("Sector"),
                                       represent="%(name)s",
                                       widget="multiselect",
@@ -371,9 +360,28 @@ def customize_org_organisation(**attr):
                                       ),
                       ]
 
+    # Custom Report Fields
+    report_fields = ["name",
+                     (T("Sectors"), "sector_organisation.sector_id"),
+                     (T("Services"), "service_organisation.service_id"),
+                     ]
+
+    report_options = Storage(#search = filter_widgets,
+                             rows = report_fields,
+                             cols = report_fields,
+                             fact = report_fields,
+                             defaults = Storage(rows = "service_organisation.service_id",
+                                                cols = "sector_organisation.sector_id",
+                                                fact = "name",
+                                                aggregate = "list",
+                                                totals = True
+                                                )
+                             )
+
     s3db.configure(tablename,
                    crud_form = crud_form,
                    filter_widgets = filter_widgets,
+                   report_options = report_options,
                    )
 
     attr["hide_filter"] = False
@@ -467,7 +475,7 @@ def customize_org_facility(**attr):
                                       represent="%(name)s",
                                       widget="multiselect",
                                       ),
-                      S3OptionsFilter("facility_type_id",
+                      S3OptionsFilter("site_facility_type.facility_type_id",
                                       label=T("Type"),
                                       represent="%(name)s",
                                       widget="multiselect",
@@ -483,6 +491,8 @@ def customize_org_facility(**attr):
                    crud_form = crud_form,
                    filter_widgets = filter_widgets,
                    )
+
+    attr["hide_filter"] = False
 
     current.response.s3.crud_strings[tablename] = Storage(
                 title_create = T("Add Location"),
