636c053bc21d47441b577f38fced09dc49837b98
Dominic König <dominic@nursix.org>
2013-08-14 11:36:44 +0200

    Bug fix / modernization.

--- modules/s3/s3widgets.py
+++ modules/s3/s3widgets.py
@@ -2696,6 +2696,7 @@ class S3LocationSelectorWidget(FormWidget):
 
     def __call__(self, field, value, **attributes):
 
+        print "here"
         T = current.T
         db = current.db
         s3db = current.s3db
