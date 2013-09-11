5d7826025277f965aa7b1a5dc1a04ad2c166a93f
Dominic König <dominic@nursix.org>
2013-09-11 23:11:19 +0200

    Use default sheet name in xls2tree before falling back to index 0

--- modules/s3/s3xml.py
+++ modules/s3/s3xml.py
@@ -2191,6 +2191,8 @@ class S3XML(S3Codec):
         COL = TAG.col
         SubElement = etree.SubElement
 
+        DEFAULT_SHEET_NAME = "SahanaData"
+
         # Root element
         root = etree.Element(TAG.table)
         if resourcename is not None:
@@ -2221,7 +2223,10 @@ class S3XML(S3Codec):
                 elif isinstance(sheet, basestring):
                     s = wb.sheet_by_name(sheet)
                 elif sheet is None:
-                    s = wb.sheet_by_index(0)
+                    if DEFAULT_SHEET_NAME in wb.sheet_names():
+                        s = wb.sheet_by_name(DEFAULT_SHEET_NAME)
+                    else:
+                        s = wb.sheet_by_index(0)
                 else:
                     raise SyntaxError("xls2tree: invalid sheet %s" % sheet)
             except IndexError, xlrd.XLRDError:
