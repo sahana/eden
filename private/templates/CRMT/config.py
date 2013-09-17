276ff3ccfbd50d99a2af5b23ff562e9ba3253e7c
Dominic König <dominic@nursix.org>
2013-09-17 11:31:02 +0200

    CRMT: Deactivate charts tab until ready, fix map being partially hidden behind filter form

--- private/templates/CRMT/config.py
+++ private/templates/CRMT/config.py
@@ -238,10 +238,10 @@ settings.ui.summary = [{"common": True,
                         "label": "Table",
                         "widgets": [{"method": "datatable"}]
                         },
-                       {"name": "charts",
-                        "label": "Charts",
-                        "widgets": [{"method": "report2"}]
-                        },
+                       #{"name": "charts",
+                        #"label": "Charts",
+                        #"widgets": [{"method": "report2"}]
+                        #},
                        {"name": "map",
                         "label": "Map",
                         "widgets": [{"method": "map", "ajax_init": True}],
