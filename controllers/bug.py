32325ed1f8e348dac16694a30a4adea355e53332
Anurag Sharma <anssharma61@gmail.com>
2013-11-20 17:09:41 +0530

    Update and rename report.py to bug.py

---
+++ controllers/bug.py
@@ -0,0 +1,6 @@
+#Controller for the report module . 
+
+def bug():
+	 return s3_rest_controller()  # A function to manage CRUD . 
+def index():
+    return dict()
--- controllers/report.py
+++
@@ -1,6 +0,0 @@
-#Controller for the report module . 
-
-def new():
-	  return s3_rest_controller()  # A function to manage CRUD . 
-def index():
-    return dict()
