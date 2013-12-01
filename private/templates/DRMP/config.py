95552c8cc6e4d8c4d6d2b570029a7810197caacc
Michael Howden <michael@sahanafoundation.org>
2013-12-02 11:06:24 +1300

    * Fix Error in Cancel Button * Fix 2 error in Profile forms * PH Menu: News -> News Feed (More explicit language)

--- private/templates/DRMP/config.py
+++ private/templates/DRMP/config.py
@@ -2347,7 +2347,7 @@ def customize_cms_post(**attr):
                            )
 
             # Breaks bootstrap formstyle & doesn't work well as just 'True'
-            s3.cancel = True
+            s3.cancel = URL(c="cms", f="post")
         elif r.representation == "xls":
             table = r.table
             table.created_by.represent = s3_auth_user_represent_name
@@ -3095,7 +3095,7 @@ def customize_org_office(**attr):
                 location_field.widget = S3LocationSelectorWidget2(levels=("L1", "L2"),
                                                                   show_address=True,
                                                                   show_map=False)
-            s3.cancel = True
+            s3.cancel = URL(c="org", f="office")
 
         return True
     s3.prep = custom_prep
@@ -3469,7 +3469,7 @@ def customize_org_resource(**attr):
                            list_layout = render_resources,
                            )
 
-            s3.cancel = True
+            s3.cancel = URL(c="org", f="resource")
 
         return True
     s3.prep = custom_prep
@@ -4029,7 +4029,7 @@ def customize_project_project(**attr):
                            filter_widgets = filter_widgets,
                            )
 
-            s3.cancel = True
+            s3.cancel = URL(c="project", f="project")
 
         return True
     s3.prep = custom_prep
