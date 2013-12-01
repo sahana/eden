5005a8daba674958244092c65467dc37dc780d5e
Anurag Sharma <anssharma61@gmail.com>
2013-12-01 07:11:06 +0530

    Create search_item.py

---
+++ modules/tests/inv/search_item.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+""" Sahana Eden Items Search Module Automated Tests
+
+    @copyright: 2011-2012 (c) Sahana Software Foundation
+    @license: MIT
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+
+
+from tests.web2unittest import SeleniumUnitTest
+
+def _kwsearch(instance, column, items, keyword):
+    for item in [instance.dt_data_item(i, column) for i in xrange(1, items + 1)]:
+        if not (keyword.strip().lower() in item.strip().lower()):
+            return False
+    return True
+
+class SearchItems(SeleniumUnitTest):
+    def setup(self):
+        super(SeleniumUnitTest, self).setUp()
+        print "\n"
+        self.login(account="admin", nexturl="supply/item/search")
+
+
+    def test_inv018_01_inv_search_simple(self):
+        """
+            @case: inv018
+            @description: Search Items - Simple Search
+        """
+        key="Air"
+        self.search(self.search.simple_form,
+            True,
+            [],
+            {"tablename":"inv_inv_item", "key":key, "filters":[("type",1)]},
+            manual_check=functools.partial(_kwsearch, keyword=key, items=1, column=2)
+        )
