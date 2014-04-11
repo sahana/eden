9bc9eb54db1be73c2405e27985a349b820f98a22
Dominic König <dominic@nursix.org>
2014-04-11 11:33:15 +0200

    DRY: S3HierarchyFilter uses S3HierarchySelectWidget

--- modules/s3/s3filter.py
+++ modules/s3/s3filter.py
@@ -56,7 +56,7 @@ from s3rest import S3Method
 from s3resource import S3FieldSelector, S3ResourceField, S3URLQuery
 from s3utils import s3_get_foreign_key, s3_unicode, S3TypeConverter
 from s3validators import *
-from s3widgets import S3DateWidget, S3DateTimeWidget, S3GroupedOptionsWidget, S3MultiSelectWidget, S3OrganisationHierarchyWidget, S3RadioOptionsWidget, S3SelectChosenWidget
+from s3widgets import S3DateWidget, S3DateTimeWidget, S3GroupedOptionsWidget, S3MultiSelectWidget, S3OrganisationHierarchyWidget, S3RadioOptionsWidget, S3SelectChosenWidget, S3HierarchySelectWidget
 
 # Compact JSON encoding
 SEPARATORS = (",", ":")
@@ -1535,8 +1535,6 @@ class S3HierarchyFilter(S3FilterWidget):
 
             lookup              name of the lookup table
             represent           representation method for the key
-
-        @status: experimental
     """
 
     _class = "hierarchy-filter"
@@ -1551,49 +1549,7 @@ class S3HierarchyFilter(S3FilterWidget):
             @param resource: the resource
             @param values: the search values from the URL query
         """
-
-        attr = self._attr(resource)
-        opts = self.opts
-        name = attr["_name"]
-
-        widget_id = attr["_id"]
         
-        rfield = None
-        selector = self.field
-        
-        lookup = opts.get("lookup")
-        if not lookup:
-            if resource:
-                rfield = S3ResourceField(resource, selector)
-                if rfield.field:
-                    lookup = s3_get_foreign_key(rfield.field, m2m=False)[0]
-            if not lookup:
-                raise SyntaxError("No lookup table known for %s" % selector)
-
-        represent = opts.get("represent")
-        if not represent:
-            if not rfield:
-                rfield = S3ResourceField(resource, selector)
-                if rfield.field:
-                    represent = rfield.field.represent
-
-        from s3hierarchy import S3Hierarchy
-        h = S3Hierarchy(tablename=lookup, represent=represent)
-
-        if not h.config:
-            raise AttributeError("No hierarchy configured for %s" % lookup)
-
-        widget = DIV(INPUT(_type="hidden",
-                           _class="s3-hierarchy-input"),
-                     DIV(h.html("%s-tree" % widget_id),
-                         _class="s3-hierarchy-tree"),
-                     **attr)
-        widget.add_class(self._class)
-
-        s3 = current.response.s3
-        scripts = s3.scripts
-        script_dir = "/%s/static/scripts" % current.request.application
-
         # Currently selected values
         selected = []
         append = selected.append
@@ -1603,27 +1559,17 @@ class S3HierarchyFilter(S3FilterWidget):
             if isinstance(v, (int, long)) or str(v).isdigit():
                 append(v)
 
-        if s3.debug:
-            script = "%s/jquery.jstree.js" % script_dir
-            if script not in scripts:
-                scripts.append(script)
-            script = "%s/S3/s3.jquery.ui.hierarchicalopts.js" % script_dir
-            if script not in scripts:
-                scripts.append(script)
-        else:
-            script = "%s/S3/s3.jstree.min.js" % script_dir
-            if script not in scripts:
-                scripts.append(script)
+        # Resolve the field selector
+        rfield = S3ResourceField(resource, self.field)
 
-        script = '''
-$('#%(widget_id)s').hierarchicalopts({
-    appname: '%(appname)s',
-    selected: %(selected)s
-});''' % {"appname": current.request.application,
-          "widget_id": widget_id,
-          "selected": json.dumps(selected, separators=SEPARATORS) if selected else "null",
-          }
-        s3.jquery_ready.append(script)
+        # Instantiate the widget
+        opts = self.opts
+        w = S3HierarchySelectWidget(lookup = opts.get("lookup"),
+                                    represent = opts.get("represent"))
+
+        # Render the widget
+        widget = w(rfield.field, selected, **self._attr(resource))
+        widget.add_class(self._class)
 
         return widget
 
