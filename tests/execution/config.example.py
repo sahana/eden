1b2b9beac70bfa83eb3d070bdfb21bca24644c09
arnavsharma93 <arnavsharma93@gmail.com>
2014-05-11 23:18:57 +0530

    Integrating redsin's edentest as RobotEden in Eden

---
+++ tests/execution/config.example.py
@@ -0,0 +1,12 @@
+# Configuration file for FunctionalTests of RobotEden
+
+# The server name, e.g. "localhost:8000"
+SERVER = "localhost:8000"
+# The web2py application name, e.g. "eden"
+APPNAME = "eden"
+# The browser, e.g. "Firefox" or "Chrome"
+BROWSER = "Chrome"
+# The user name to test with
+VALID_USER = "admin@example.com"
+# The password for the test user
+VALID_PASSWORD = "testing"
