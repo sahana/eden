b16407c90a7fde55228f1b3305e64a764a23d256
Fran Boon <fran@aidiq.com>
2014-08-11 14:11:48 +0200

    Cleanup

--- modules/s3/s3data.py
+++ modules/s3/s3data.py
@@ -51,7 +51,7 @@ from gluon.html import *
 from gluon.storage import Storage
 from gluon.validators import IS_EMPTY_OR, IS_IN_SET
 
-from s3utils import s3_flatlist, s3_has_foreign_key, s3_orderby_fields, s3_truncate, s3_unicode, S3MarkupStripper, s3_represent_value, s3_set_extension
+from s3utils import s3_flatlist, s3_has_foreign_key, s3_orderby_fields, s3_truncate, s3_unicode, S3MarkupStripper, s3_represent_value
 from s3validators import IS_NUMBER
 
 DEBUG = False
@@ -677,11 +677,10 @@ class S3DataTable(object):
                   f=request.function,
                   args=request.args,
                   vars=request.get_vars,
-                 )
+                  )
         _ajaxUrl = s3_set_extension( url, "aadata")           
         config.ajaxUrl = attr.get("dt_ajax_url", _ajaxUrl)
         config.rowStyles = attr.get("dt_styles", [])
-
 
         rowActions = attr.get("dt_row_actions", s3.actions)
         if rowActions:
