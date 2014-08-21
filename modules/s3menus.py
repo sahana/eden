785821d694597bcba269928e904a7b02c83551e4
Gaurav Mittal <gaurav.mittal@students.iiit.ac.in>
2014-08-21 17:07:48 +0530

    Reloacating the logout button

--- modules/s3menus.py
+++ modules/s3menus.py
@@ -215,7 +215,6 @@ class S3MainMenu(object):
             menu_auth = MM(auth.user.email, c="default", f="user",
                            translate=False, link=False, _id="auth_menu_email",
                            **attr)(
-                            MM("Logout", m="logout", _id="auth_menu_logout"),
                             MM("User Profile", m="profile"),
                             MM("Personal Data", c="default", f="person", m="update"),
                             MM("Contact Details", c="pr", f="person",
@@ -225,6 +224,7 @@ class S3MainMenu(object):
                                 #args="pe_subscription",
                                 #vars={"person.pe_id" : auth.user.pe_id}),
                             MM("Change Password", m="change_password"),
+                            MM("Logout", m="logout", _id="auth_menu_logout"),
                             SEP(),
                             MM({"name": current.T("Rapid Data Entry"),
                                "id": "rapid_toggle",
