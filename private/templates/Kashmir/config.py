2de0ef887b9694a752a7024001239faf202e925f
Dominic König <dominic@nursix.org>
2014-09-22 17:07:52 +0200

    Kashmir: Add shelter permissions for OrgAdmins, use policy 7 (hierarchical realms)

--- private/templates/Kashmir/config.py
+++ private/templates/Kashmir/config.py
@@ -93,7 +93,7 @@ settings.L10n.utc_offset = "UTC +0530"
 # 7: Apply Controller, Function, Table ACLs and Entity Realm + Hierarchy
 # 8: Apply Controller, Function, Table ACLs, Entity Realm + Hierarchy and Delegations
 #
-settings.security.policy = 6 # Organisation-ACLs
+settings.security.policy = 7
 
 # RSS feeds
 settings.frontpage.rss = []
