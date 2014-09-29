51bc9afc6345d90c48f87bb68402fb3666eec10b
Dominic König <dominic@nursix.org>
2014-09-29 20:19:44 +0200

    Move tests/ci_bin => tests/travis

--- tests/travis/generate_requirements_file.py
+++ tests/travis/generate_requirements_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # usage - python generate_requirements.py [folder where the file should be generated] [list of requirements file]
-# example - python tests/ci_bin/generate_requirements_file.py tests/ci_bin requirements.txt optional_requirements.txt
+# example - python tests/travis/generate_requirements_file.py tests/travis requirements.txt optional_requirements.txt
 
 from sys import argv
 
