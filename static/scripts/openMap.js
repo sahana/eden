cc72787e46519a2eed4456221acda94805b9b545
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-12-02 21:52:09 -0500

    Fixing js and css isuues with display of map and directions changing type of ShowMap from Button to Link

--- static/scripts/openMap.js
+++ static/scripts/openMap.js
@@ -1,45 +1,44 @@
 var opened = false;
-var dialog;
 function openMap(start, end){
-    if(!opened){
-        dialog = document.createElement("DIALOG");
-        $(dialog).width("800px");
-        $(dialog).height("700px");
-        $(dialog).html('<div id="map-canvas" style="width:400px; height:600px;"></div><div id="directions" style="width:200px; height:600px;"></div>');
-        $(dialog).appendTo('body');
-        $(dialog).appendTo('body');
-        setmap(start,end);
-        opened = true;
-    }
-    dialog.showModal();
+  if( !opened )
+  {
+    var dialog = document.createElement('div');
+    $(dialog).html('<div id="map-canvas" style="width:95%;height:400px;"></div> <div id="map-dirs" style="width:95%;height:40%;"></div>');
+    $(dialog).width('500px');
+    $(dialog).height('800px');
+    dialog.id = 'map-dialog';
+    document.body.appendChild(dialog);
+    setmap(start,end);
+    opened = true;
+  }
+  $('#map-dialog').dialog({title: 'Directions',maxHeight: 800});
 }
 
-			function setmap(st, en) {
-			var map;
-				var directionsService = new google.maps.DirectionsService();
-				var directionsDisplay = new google.maps.DirectionsRenderer();
+function setmap(st, en) {
+  var map;
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
 
-				//set lat lang to Raleigh by default
-				var latlng = new google.maps.LatLng(35.779943, -78.641617);
-				var mapOptions = {
-					zoom: 10,
-					center: latlng
-				}
-				map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-				//if worksite address is set, show it on the map
-					directionsDisplay.setMap(map);
-					directionsDisplay.setPanel(document.getElementById("directions"));
-					var start = st;	//Worksite address
-					var end = en;
-					var request = {
-						origin:st,
-          				destination:en,
-         				travelMode: google.maps.DirectionsTravelMode.DRIVING,
-         				unitSystem: google.maps.UnitSystem.IMPERIAL
-      				};
-					directionsService.route(request, function(response, status) {
-						if (status == google.maps.DirectionsStatus.OK) {
-							directionsDisplay.setDirections(response);
-						}
-					});
-			}
+  //set lat lang to Raleigh by default
+  var latlng = new google.maps.LatLng(35.779943, -78.641617);
+  var mapOptions = {
+    zoom: 10,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  //if worksite address is set, show it on the map
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById("map-dirs"));
+  var request = {
+  origin:st,
+  destination:en,
+  travelMode: google.maps.DirectionsTravelMode.DRIVING,
+  unitSystem: google.maps.UnitSystem.IMPERIAL
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+       directionsDisplay.setDirections(response);
+    }
+  });
+}
