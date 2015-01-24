51a66308e1b151761c60b05a16f87796e8c059fc
raj454raj <raj454raj@gmail.com>
2015-01-24 21:44:53 +0530

    Tweaks

--- modules/s3menus.py
+++ modules/s3menus.py
@@ -913,7 +913,7 @@ class S3OptionsMenu(object):
                     # Currently not got geocoding support
                     #M("Bulk Uploader", c="doc", f="bulk_upload"),
                     M("Locations", c="gis", f="location")(
-                        M("Create Location", m="create"),
+                        M("Create", m="create"),
                         #M("Create Location Group", m="create", vars={"group": 1}),
                         M("Import from CSV", m="import", restrict=[MAP_ADMIN]),
                         M("Import from OpenStreetMap", m="import_poi",
@@ -1244,7 +1244,7 @@ class S3OptionsMenu(object):
 
         return M(c="irs")(
                     M("Incident Reports", f="ireport")(
-                        M("Create Incident Report", m="create"),
+                        M("Create", m="create"),
                         M("Open Incidents", vars={"open":1}),
                         M("Map", m="map"),
                         M("Timeline", args="timeline"),
@@ -1365,12 +1365,12 @@ class S3OptionsMenu(object):
 
         return M(c="member")(
                     M("Members", f="membership", m="summary")(
-                        M("Create Member", m="create"),
+                        M("Create", m="create"),
                         #M("Report", m="report"),
                         M("Import", f="person", m="import"),
                     ),
                     M("Membership Types", f="membership_type")(
-                        M("Create Membership Type", m="create"),
+                        M("Create", m="create"),
                         #M("Import", m="import"),
                     ),
                 )
@@ -1736,7 +1736,6 @@ class S3OptionsMenu(object):
                     M("Detail", f="details", restrict=[ADMIN]),
                     M("User", f="user", restrict=[ADMIN]),
                 )
-
 
     # -------------------------------------------------------------------------
     @staticmethod
