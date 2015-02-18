e42f35d3863bbe24aac01e685b99031ea48fd548
Dominic König <dominic@nursix.org>
2015-02-18 21:19:18 +0100

    RMS: realm rules for hrm_training

--- modules/templates/IFRC/config.py
+++ modules/templates/IFRC/config.py
@@ -101,6 +101,7 @@ def config(settings):
                                 pr_education = PID,
                                 pr_note = PID,
                                 hrm_human_resource = SID,
+                                hrm_training = PID,
                                 inv_recv = SID,
                                 inv_send = SID,
                                 inv_track_item = "track_org_id",
@@ -167,6 +168,28 @@ def config(settings):
                                      limitby=(0, 1)).first()
             if otype and otype.name != "Red Cross / Red Crescent":
                 use_user_organisation = True
+
+        elif tablename == "hrm_training":
+            # Inherit realm entity from the related HR record
+            htable = s3db.hrm_human_resource
+            query = (table.id == row.id) & \
+                    (htable.person_id == table.person_id) & \
+                    (htable.deleted != True)
+            rows = db(query).select(htable.realm_entity, limitby=(0, 2))
+            if len(rows) == 1:
+                realm_entity = rows.first().realm_entity
+            else:
+                # Ambiguous => try course organisation
+                ctable = s3db.hrm_course
+                otable = s3db.org_organisation
+                query = (table.id == row.id) & \
+                        (ctable.id == table.course_id) & \
+                        (otable.id == ctable.organisation_id)
+                row = db(query).select(otable.pe_id,
+                                       limitby=(0, 1)).first()
+                if row:
+                    realm_entity = row.pe_id
+                # otherwise: inherit from the person record
 
         # Groups are owned by the user's organisation
         #elif tablename in ("pr_group",):
