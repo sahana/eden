81d4ca5b25b7a781adb3c454227f47437b2d8fa1
Thishani <thishanilucas@gmail.com>
2015-02-18 20:23:23 +0530

    Started The Finance Module
    Nothing's workin................

--- controllers/finance.py
+++ controllers/finance.py
@@ -1,2 +1,29 @@
 def donations():
 	return s3_rest_controller()
+	
+def index():
+	return dict()
+	
+s3.crud_strings["finance_donations"] = Storage(
+label_create = T("Create New Donation"),
+title_display = T("Donations Details"),
+title_list = T("List Donations"),
+title_update = T("Edit Donation"),
+title_upload = T("Import Donations"),
+subtitle_list = T("Donations"),
+label_list_button = T("List Donations"),
+label_delete_button = T("Delete Donation"),
+msg_record_created = T("Donation added"),
+msg_record_modified = T("Donation updated"),
+msg_record_deleted = T("Donation deleted"),
+msg_list_empty = T("No Donations currently registered"))
+
+
+def donations_total(r, **attr):
+	table = current.s3db.finance_donations
+	tot = table.amount.sum()
+	query = (table.deleted != True) 
+	return current.db(query).select(tot).first()[tot] or \
+               current.messages["NONE"]
+               
+	
--- modules/s3db/finance.py
+++ modules/s3db/finance.py
@@ -1,5 +1,53 @@
-tablename = "finance_donations"
-db.define_table(tablename,
-				Field("Donar"),
-				Field("Amount"),
-				)
+__all__ = ["FinanceDonations",
+           
+           ]
+
+from gluon import *
+from gluon.storage import Storage
+from ..s3 import *
+from s3layouts import S3AddResourceLink
+
+
+class FinanceDonations(S3Model):
+	names = ["finance_donations",
+             "finance_donations_id",
+			 
+			 ]
+			 
+	def model(self):
+
+        # You will most likely need (at least) these:
+			db = current.db
+			T = current.T
+
+        # This one may be useful:
+			settings = current.deployment_settings
+	
+
+			tablename = "finance_donations"
+			table=self.define_table(tablename,
+							Field("donar", label=T("Donar"),),
+							Field("amount", "double", label=T("Amount"),
+									default=0.00, 
+									requires = IS_FLOAT_AMOUNT(),
+									),
+							s3_currency(required=True),
+							)
+			
+			self.configure(tablename,
+						   listadd=True)
+						   
+			finance_donations_id = S3ReusableField("finance_donations_id", table,
+                                               label = T("Finance Donations"),
+                                               requires = IS_NULL_OR(IS_ONE_OF(db,
+                                                                     "finance_donations.id")))
+
+        # Pass names back to global scope (s3.*)
+			return dict(
+				finance_donations_id=finance_donations_id,
+			)
+			
+			
+			self.set_method("finance", "donations", method="total", action=donations_total)
+			
+	
