73dcc595d522a950fcac0e7cff72698a680dd983
Dominic König <dominic@nursix.org>
2015-05-20 23:08:26 +0200

    Re-apply trunk8 after datatable filtering (analogous to data lists)

--- static/scripts/S3/S3.js
+++ static/scripts/S3/S3.js
@@ -350,6 +350,14 @@ S3.trunk8 = function(selector, lines, more) {
             }
         });
     });
+    // Attach to any new items after Ajax-listUpdate (dataTables)
+    $('.dataTable').on('draw.dt', function() {
+        $(this).find(selector).each(function() {
+            if (this.trunk8 === undefined) {
+                $(this).trunk8(settings);
+            }
+        });
+    });
 };
 
 // ============================================================================
