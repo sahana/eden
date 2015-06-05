7dad9c081f0d28b9e85704971751b9c8fc267100
Dominic König <dominic@nursix.org>
2015-06-05 08:31:41 +0200

    Adding PostgreSQL-9.4 to CI script (Debian 8 Jessie)

--- tests/travis/configure_db.sh
+++ tests/travis/configure_db.sh
@@ -13,34 +13,45 @@ if [[ $DB == mysql ]]; then
 
 elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
 
-
     if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.1 ]]; then
         echo "Setting up postgres version 9.1"
         sudo /etc/init.d/postgresql stop
         sudo /etc/init.d/postgresql start 9.1
         psql -c "create database sahana;" -U postgres
+    fi
 
-
-    else
+    if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.3 ]]; then
         echo "Setting up postgres version 9.3"
         sudo /etc/init.d/postgresql stop
         sudo /etc/init.d/postgresql start 9.3
         psql -c "create database sahana;" -U postgres
 
         if [[ $DB == postgres-9.3+postgis ]]; then
-
             psql -U postgres -d sahana -c "create extension postgis"
             psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
             psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
-
             sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
-
         fi
 
         echo "pg8000 (default web2py postgres adapter) does not support some postgres 9.3 features. Using psycopg2 instead."
-
         sudo pip install psycopg2 -q
+    fi
 
+    if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.4 ]]; then
+        echo "Setting up postgres version 9.4"
+        sudo /etc/init.d/postgresql stop
+        sudo /etc/init.d/postgresql start 9.4
+        psql -c "create database sahana;" -U postgres
+
+        if [[ $DB == postgres-9.4+postgis ]]; then
+            psql -U postgres -d sahana -c "create extension postgis"
+            psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
+            psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
+            sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
+        fi
+
+        echo "pg8000 (default web2py postgres adapter) does not support some postgres 9.4 features. Using psycopg2 instead."
+        sudo pip install psycopg2 -q
     fi
 
     sed -ie 's|\#settings.database.db_type = "postgres"|settings.database.db_type = "postgres"|' models/000_config.py
