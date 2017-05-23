fd7e828e774881701f6288f88b339090f351cb50
tirgil <tirgil@gmail.com>
2017-05-23 12:15:45 +0300

    Add Phone to beneficiary list and remove xls check, because it is not necessary anymore

--- modules/templates/STL/config.py
+++ modules/templates/STL/config.py
@@ -2511,6 +2511,7 @@ def config(settings):
                                    "first_name",
                                    #"middle_name",
                                    "last_name",
+                                   (T("Phone"), "phone.value"),
                                    "date_of_birth",
                                    "gender",
                                    "person_details.nationality",
@@ -2519,9 +2520,7 @@ def config(settings):
                                    "dvr_case.date",
                                    "dvr_case.status_id",
                                    ]
-                    if r.representation == "xls":
-                        list_fields.append(("Phone","phone.value"))
-
+                                                       
                     resource.configure(list_fields = list_fields,
                                        )
 
