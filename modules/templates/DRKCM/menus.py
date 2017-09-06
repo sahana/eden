984584ab1658b569d50795934331eee7d9a5de79
Dominic König <dominic@nursix.org>
2017-09-06 11:03:00 +0200

    DRKCM: Ability for OrgGroupAdmins to manage residence status categories

--- modules/templates/DRKCM/menus.py
+++ modules/templates/DRKCM/menus.py
@@ -273,6 +273,8 @@ class S3OptionsMenu(default.S3OptionsMenu):
                         M("Need Types", f="need"),
                         M("Intervention Types", f="response_type", m="hierarchy"),
                         M("Appointment Types", f="case_appointment_type"),
+                        M("Residence Status Types", f="residence_status_type"),
+                        M("Residence Permit Types", f="residence_permit_type"),
                         ),
                     )
 
