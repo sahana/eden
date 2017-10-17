5d3f19888aea8062659088dfd693011658677450
Biplov Bhandari <bionicbiplov45@gmail.com>
2017-10-17 16:07:54 +0700

    bugfix + set country sepecific info segment language

--- modules/templates/SAMBRO/NP/config.py
+++ modules/templates/SAMBRO/NP/config.py
@@ -25,6 +25,7 @@ def config(settings):
         ("en-US", "English"),
         ("ne", "Nepali"),
     ])
+    settings.cap.languages = languages
     settings.L10n.languages = languages
 
     # Alert Hub Title
