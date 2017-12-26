66a2ec2e54d19f47b9aa9f664f2116388deacb7f
Dominic König <dominic@nursix.org>
2017-12-26 13:11:51 +0100

    S3ResourceData.filter_query: count distinct

--- modules/s3/s3resource.py
+++ modules/s3/s3resource.py
@@ -6085,7 +6085,7 @@ class S3ResourceData(object):
             totalids = len(rows)
             if limit and totalids >= maxids or start != 0 and not totalids:
                 # Count all matching records
-                cnt = table._id.count()
+                cnt = table._id.count(distinct=True)
                 row = db(query).select(cnt,
                                        join = join,
                                        left = left,
