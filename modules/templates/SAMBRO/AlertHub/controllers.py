3a8b2cbc6337cdd681ce3dc04c1e78dccd3ad00c
Nuwan Waidyanatha <waidyanatha@gmail.com>
2018-01-15 22:05:14 +0800

    New AlertHub template with relevant prepop files and change to Demo

---
+++ modules/templates/SAMBRO/AlertHub/controllers.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from templates.SAMBRO.controllers import index, subscriptions, user_info, alert_hub_cop
+
+class alert_map(index):
+    pass
+
+class index(alert_hub_cop):
+    pass
