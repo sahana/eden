081c6247e656816cb80b4a4d675492bb39c3ce34
Dominic König <dominic@nursix.org>
2018-01-23 11:09:40 +0100

    Travis CI config: switch to Trusty, retire postgresql-9.1/9.3 configurations (adding 9.6 instead)

--- tests/travis/configure_db.sh
+++ tests/travis/configure_db.sh
@@ -13,30 +13,6 @@ if [[ $DB == mysql ]]; then
 
 elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
 
-    if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.1 ]]; then
-        echo "Setting up postgres version 9.1"
-        sudo /etc/init.d/postgresql stop
-        sudo /etc/init.d/postgresql start 9.1
-        psql -c "create database sahana;" -U postgres
-    fi
-
-    if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.3 ]]; then
-        echo "Setting up postgres version 9.3"
-        sudo /etc/init.d/postgresql stop
-        sudo /etc/init.d/postgresql start 9.3
-        psql -c "create database sahana;" -U postgres
-
-        if [[ $DB == postgres-9.3+postgis ]]; then
-            psql -U postgres -d sahana -c "create extension postgis"
-            psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
-            psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
-            sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
-        fi
-
-        echo "pg8000 (default web2py postgres adapter) does not support some postgres 9.3 features. Using psycopg2 instead."
-        sudo pip install psycopg2 -q
-    fi
-
     if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.4 ]]; then
         echo "Setting up postgres version 9.4"
         sudo /etc/init.d/postgresql stop
@@ -44,6 +20,8 @@ elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
         psql -c "create database sahana;" -U postgres
 
         if [[ $DB == postgres-9.4+postgis ]]; then
+            sudo apt-get -qq update
+            sudo apt-get install -y postgresql-9.4-postgis-2.3
             psql -U postgres -d sahana -c "create extension postgis"
             psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
             psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
@@ -51,6 +29,25 @@ elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
         fi
 
         echo "pg8000 (default web2py postgres adapter) does not support some postgres 9.4 features. Using psycopg2 instead."
+        sudo pip install psycopg2 -q
+    fi
+
+    if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.6 ]]; then
+        echo "Setting up postgres version 9.6"
+        sudo /etc/init.d/postgresql stop
+        sudo /etc/init.d/postgresql start 9.6
+        psql -c "create database sahana;" -U postgres
+
+        if [[ $DB == postgres-9.6+postgis ]]; then
+            sudo apt-get -qq update
+            sudo apt-get install -y postgresql-9.6-postgis-2.3
+            psql -U postgres -d sahana -c "create extension postgis"
+            psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
+            psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
+            sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
+        fi
+
+        echo "pg8000 (default web2py postgres adapter) does not support some postgres 9.6 features. Using psycopg2 instead."
         sudo pip install psycopg2 -q
     fi
 
