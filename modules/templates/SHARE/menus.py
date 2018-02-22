cf1d283367844b4abf7a862f320e9c7a0e21432d
Nuwan Waidyanatha <waidyanatha@gmail.com>
2018-02-22 13:44:06 +0800

    SHARE template first cut

---
+++ modules/templates/SHARE/menus.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from gluon import current
+from s3 import *
+from s3layouts import *
+try:
+    from .layouts import *
+except ImportError:
+    pass
+import s3menus as default
+
+# =============================================================================
+class S3MainMenu(default.S3MainMenu):
+    """ Custom Application Main Menu """
+
+    # -------------------------------------------------------------------------
+    @classmethod
+    def menu_modules(cls):
+        """ Custom Modules Menu """
+
+        menu= [MM("Requests", c="req", f="req")(
+                MM("Commitments", c="req", f="commit"),
+                ),
+               MM("Aid & Supplies", c="supply", f="item", m="summary")(
+                MM("Item Catalog", c="supply", f="catalog_item"),
+                MM("Assets", c="asset", f="asset"),
+                ),
+               MM("Sitreps", c="cms", f="blog/1"),
+               MM("more", link=False)(
+                MM("Organizations", c="org", f="organisation"),
+                MM("Facilities", c="org", f="facility"),
+                MM("Documents", c="doc", f="document"),
+                MM("Events", c="event", f="event"),
+                ),
+               ]
+
+        return menu
+
+# END =========================================================================
