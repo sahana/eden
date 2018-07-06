226a8eb65b177812e677583b9cf0fb9f2fb7af3c
Dominic König <dominic@nursix.org>
2018-07-06 13:19:57 +0200

    Start implementing SHARE design incl homepage (WIP)

--- modules/templates/SHARE/menus.py
+++ modules/templates/SHARE/menus.py
@@ -15,6 +15,24 @@ class S3MainMenu(default.S3MainMenu):
 
     # -------------------------------------------------------------------------
     @classmethod
+    def menu(cls):
+        """ Compose Menu """
+
+        # Modules menus
+        main_menu = MM()(
+            cls.menu_modules(),
+        )
+
+        # Additional menus
+        current.menu.personal = cls.menu_personal()
+        current.menu.lang = cls.menu_lang()
+        current.menu.about = cls.menu_about()
+        current.menu.org = cls.menu_org()
+
+        return main_menu
+
+    # -------------------------------------------------------------------------
+    @classmethod
     def menu_modules(cls):
         """ Custom Modules Menu """
 
@@ -45,6 +63,106 @@ class S3MainMenu(default.S3MainMenu):
                ]
 
         return menu
+
+    # -------------------------------------------------------------------------
+    @classmethod
+    def menu_org(cls):
+        """ Organisation Logo and Name """
+
+        OM = S3OrgMenuLayout
+        return OM()
+
+    # -------------------------------------------------------------------------
+    @classmethod
+    def menu_lang(cls):
+        """ Language Selector """
+
+        languages = current.deployment_settings.get_L10n_languages()
+        represent_local = IS_ISO639_2_LANGUAGE_CODE.represent_local
+
+        menu_lang = ML("Language", right=True)
+
+        for code in languages:
+            # Show each language name in its own language
+            lang_name = represent_local(code)
+            menu_lang(
+                ML(lang_name,
+                   translate = False,
+                   lang_code = code,
+                   lang_name = lang_name,
+                   )
+            )
+
+        return menu_lang
+
+    # -------------------------------------------------------------------------
+    @classmethod
+    def menu_personal(cls):
+        """ Personal Menu """
+
+        auth = current.auth
+        s3 = current.response.s3
+        settings = current.deployment_settings
+
+        ADMIN = current.auth.get_system_roles().ADMIN
+
+        if not auth.is_logged_in():
+            request = current.request
+            login_next = URL(args=request.args, vars=request.vars)
+            if request.controller == "default" and \
+               request.function == "user" and \
+               "_next" in request.get_vars:
+                login_next = request.get_vars["_next"]
+
+            self_registration = settings.get_security_self_registration()
+            menu_personal = MP()(
+                        MP("Register", c="default", f="user",
+                           m = "register",
+                           check = self_registration,
+                           ),
+                        MP("Login", c="default", f="user",
+                           m = "login",
+                           vars = {"_next": login_next},
+                           ),
+                        )
+            if settings.get_auth_password_retrieval():
+                menu_personal(MP("Lost Password", c="default", f="user",
+                                 m = "retrieve_password",
+                                 ),
+                              )
+        else:
+            s3_has_role = auth.s3_has_role
+            is_org_admin = lambda i: not s3_has_role(ADMIN) and \
+                                     s3_has_role("ORG_ADMIN")
+            menu_personal = MP()(
+                        MP("Administration", c="admin", f="index",
+                           restrict = ADMIN,
+                           ),
+                        MP("Administration", c="admin", f="user",
+                           check = is_org_admin,
+                           ),
+                        MP("Profile", c="default", f="person"),
+                        MP("Change Password", c="default", f="user",
+                           m = "change_password",
+                           ),
+                        MP("Logout", c="default", f="user",
+                           m = "logout",
+                           ),
+            )
+        return menu_personal
+
+    # -------------------------------------------------------------------------
+    @classmethod
+    def menu_about(cls):
+
+        ADMIN = current.auth.get_system_roles().ADMIN
+
+        menu_about = MA(c="default")(
+            MA("Help", f="help"),
+            #MA("Contact", f="contact"),
+            MA("Version", f="about", restrict = ADMIN),
+        )
+        return menu_about
 
 # =============================================================================
 class S3OptionsMenu(default.S3OptionsMenu):
