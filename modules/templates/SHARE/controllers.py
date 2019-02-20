8bebe645410ce82e5414c4f444a163e4ac1b4046
Fran Boon <fran@aidiq.com>
2019-02-20 13:54:36 +0000

    SHARE: Editable reasources section on homepage, revert graph to approved version.

--- modules/templates/SHARE/controllers.py
+++ modules/templates/SHARE/controllers.py
@@ -3,7 +3,7 @@
 import dateutil
 import json
 
-from gluon import current, A, DIV, H3, H4, HR, LI, P, SPAN, TAG, TEXTAREA, UL, URL, XML
+from gluon import current, A, BR, DIV, H3, H4, HR, LI, P, SPAN, TAG, TEXTAREA, UL, URL, XML
 from s3 import ICON, \
                s3_auth_user_represent, s3_str, \
                S3CustomController, \
@@ -23,6 +23,8 @@ class index(S3CustomController):
         T = current.T
         db = current.db
         s3db = current.s3db
+        s3 = current.response.s3
+        session_s3 = current.session.s3
 
         output = {}
 
@@ -39,7 +41,7 @@ class index(S3CustomController):
 
         left = [etable.on(etable.id == stable.event_id)]
 
-        language = current.session.s3.language
+        language = session_s3.language
         if language != current.deployment_settings.get_L10n_default_language():
             ntable = s3db.event_event_name
             left.append(ntable.on((ntable.event_id == etable.id) & \
@@ -115,7 +117,49 @@ class index(S3CustomController):
                                             ),
                                 )
 
-        self._view(THEME, "index.html")
+        # Resources section
+        if current.deployment_settings.has_module("cms"):
+            system_roles = current.auth.get_system_roles()
+            ADMIN = system_roles.ADMIN in session_s3.roles
+            table = s3db.cms_post
+            ltable = s3db.cms_post_module
+            module = "default"
+            resource = "index"
+            query = (ltable.module == module) & \
+                    ((ltable.resource == None) | \
+                     (ltable.resource == resource)) & \
+                    (ltable.post_id == table.id) & \
+                    (table.deleted != True)
+            item = current.db(query).select(table.body,
+                                            table.id,
+                                            limitby=(0, 1)).first()
+            if item:
+                if ADMIN:
+                    item = DIV(XML(item.body),
+                               BR(),
+                               A(T("Edit"),
+                                 _href=URL(c="cms", f="post",
+                                           args=[item.id, "update"]),
+                                 _class="action-btn"))
+                else:
+                    item = DIV(XML(item.body))
+            elif ADMIN:
+                if s3.crud.formstyle == "bootstrap":
+                    _class = "btn"
+                else:
+                    _class = "action-btn"
+                item = A(T("Edit"),
+                         _href=URL(c="cms", f="post", args="create",
+                                   vars={"module": module,
+                                         "resource": resource
+                                         }),
+                         _class="%s cms-edit" % _class)
+            else:
+                item = ""
+        else:
+            item = ""
+
+        output["item"] = item
 
         # Inject D3 scripts
         from s3 import S3Report
@@ -123,7 +167,6 @@ class index(S3CustomController):
 
         # Inject charts-script
         appname = current.request.application
-        s3 = current.response.s3
         scripts = s3.scripts
         if s3.debug:
             script = "/%s/static/scripts/S3/s3.ui.charts.js" % appname
@@ -153,6 +196,8 @@ class index(S3CustomController):
             output["last_stats_update"] = T("Updated on %(date)s") % {"date": last_update}
         else:
             output["last_stats_update"] = None
+
+        self._view(THEME, "index.html")
 
         return output
 
