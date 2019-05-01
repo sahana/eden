0637fab14e182987a68ebfc52f817a763de15b6b
Dominic König <dominic@nursix.org>
2019-05-01 22:09:44 +0200

    Start consent tracking model

--- modules/s3db/auth.py
+++ modules/s3db/auth.py
@@ -27,17 +27,19 @@
     OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__all__ = ("S3AuthModel",
-           "S3AuthUserOptions",
+__all__ = ("AuthDomainApproverModel",
+           "AuthUserOptionsModel",
+           "AuthConsentModel",
            "auth_user_options_get_osm"
            )
 
 from gluon import *
 from gluon.storage import Storage
+
 from ..s3 import *
 
 # =============================================================================
-class S3AuthModel(S3Model):
+class AuthDomainApproverModel(S3Model):
 
     names = ("auth_organisation",)
 
@@ -45,18 +47,17 @@ class S3AuthModel(S3Model):
 
         T = current.T
 
-        # =============================================================================
-        # Domain table
-        #
+        # ---------------------------------------------------------------------
+        # Domain table:
         # When users register their email address is checked against this list.
-        # If the Domain matches, then they are automatically assigned to the
-        # Organization.
-        # If there is no Approvals email then the user is automatically approved.
-        # If there is an Approvals email then the approval request goes to this
-        # address
-        # If a user registers for an Organization & the domain doesn't match (or
-        # isn't listed) then the approver gets the request
-
+        #   - If the Domain matches, then they are automatically assigned to the
+        #     Organization.
+        #   - If there is no Approvals email then the user is automatically approved.
+        #   - If there is an Approvals email then the approval request goes to this
+        #     address
+        #   - If a user registers for an Organization & the domain doesn't match (or
+        #     isn't listed) then the approver gets the request
+        #
         if current.deployment_settings.get_auth_registration_requests_organisation():
             ORG_HELP = T("If this field is populated then a user who specifies this Organization when signing up will be assigned as a Staff of this Organization unless their domain doesn't match the domain field.")
         else:
@@ -68,20 +69,29 @@ class S3AuthModel(S3Model):
         tablename = "auth_organisation"
         self.define_table(tablename,
                           self.org_organisation_id(
-                                comment=DIV(_class="tooltip",
-                                            _title="%s|%s" % (current.messages.ORGANISATION,
-                                                                ORG_HELP))),
+                                comment=DIV(_class = "tooltip",
+                                            _title = "%s|%s" % (current.messages.ORGANISATION,
+                                                                ORG_HELP,
+                                                                ),
+                                            ),
+                                ),
                           Field("domain",
                                 label = T("Domain"),
-                                comment=DIV(_class="tooltip",
-                                            _title="%s|%s" % (T("Domain"),
-                                                                DOMAIN_HELP))),
+                                comment=DIV(_class = "tooltip",
+                                            _title = "%s|%s" % (T("Domain"),
+                                                                DOMAIN_HELP,
+                                                                ),
+                                            ),
+                                ),
                           Field("approver",
                                 label = T("Approver"),
                                 requires = IS_EMPTY_OR(IS_EMAIL()),
-                                comment=DIV(_class="tooltip",
-                                            _title="%s|%s" % (T("Approver"),
-                                                                APPROVER_HELP))),
+                                comment=DIV(_class = "tooltip",
+                                            _title = "%s|%s" % (T("Approver"),
+                                                                APPROVER_HELP,
+                                                                ),
+                                            ),
+                                ),
                           s3_comments(),
                           *s3_meta_fields())
 
@@ -92,7 +102,8 @@ class S3AuthModel(S3Model):
 
 
 # =============================================================================
-class S3AuthUserOptions(S3Model):
+class AuthUserOptionsModel(S3Model):
+    """ Model to store per-user configuration options """
 
     names = ("auth_user_options",)
 
@@ -100,25 +111,154 @@ class S3AuthUserOptions(S3Model):
 
         T = current.T
 
+        # ---------------------------------------------------------------------
+        # User Options
+        #
+        OAUTH_KEY_HELP = "%s|%s|%s" % (T("OpenStreetMap OAuth Consumer Key"),
+                                       T("In order to be able to edit OpenStreetMap data from within %(name_short)s, you need to register for an account on the OpenStreetMap server.") % \
+                                            {"name_short": current.deployment_settings.get_system_name_short()},
+                                       T("Go to %(url)s, sign up & then register your application. You can put any URL in & you only need to select the 'modify the map' permission.") % \
+                                            {"url": A("http://www.openstreetmap.org",
+                                                      _href="http://www.openstreetmap.org",
+                                                      _target="blank",
+                                                      ),
+                                             },
+                                       )
+
         self.define_table("auth_user_options",
                           self.super_link("pe_id", "pr_pentity"),
                           Field("user_id", current.auth.settings.table_user),
                           Field("osm_oauth_consumer_key",
                                 label = T("OpenStreetMap OAuth Consumer Key"),
                                 comment = DIV(_class="stickytip",
-                                              _title="%s|%s|%s" % (T("OpenStreetMap OAuth Consumer Key"),
-                                                                   T("In order to be able to edit OpenStreetMap data from within %(name_short)s, you need to register for an account on the OpenStreetMap server.") % \
-                                                                        dict(name_short=current.deployment_settings.get_system_name_short()),
-                                                                   T("Go to %(url)s, sign up & then register your application. You can put any URL in & you only need to select the 'modify the map' permission.") % \
-                                                                        dict(url=A("http://www.openstreetmap.org",
-                                                                                   _href="http://www.openstreetmap.org",
-                                                                                   _target="blank"))))
+                                              _title=OAUTH_KEY_HELP,
+                                              ),
                                 ),
                           Field("osm_oauth_consumer_secret",
                                 label = T("OpenStreetMap OAuth Consumer Secret"),
                                 ),
                           *s3_meta_fields())
 
+        # ---------------------------------------------------------------------
+        # Pass names back to global scope (s3.*)
+        #
+        return {}
+
+# =============================================================================
+class AuthConsentModel(S3Model):
+    """
+        Model to track consent, e.g. to legitimise processing of personal
+        data under GDPR rules.
+    """
+
+    names = ("auth_consent_option_type",
+             "auth_consent_option",
+             "auth_consent",
+             )
+
+    def model(self):
+
+        T = current.T
+
+        #db = current.db
+        s3 = current.response.s3
+
+        define_table = self.define_table
+        crud_strings = s3.crud_strings
+
+        # ---------------------------------------------------------------------
+        # Consent Option Types
+        # - types of data processing consent is requested for
+        #
+        tablename = "auth_consent_option_type"
+        define_table(tablename,
+                     # TODO code for hard-coded lookups
+                     Field("name",
+                           requires = IS_NOT_EMPTY(),
+                           ),
+                     s3_comments(),
+                     *s3_meta_fields())
+
+        # TODO CRUD Strings
+        crud_strings[tablename] = Storage(
+            label_create = T("Create Example"),
+            title_display = T("Example Details"),
+            title_list = T("Examples"),
+            title_update = T("Edit Example"),
+            label_list_button = T("List Examples"),
+            label_delete_button = T("Delete Example"),
+            msg_record_created = T("Example created"),
+            msg_record_modified = T("Example updated"),
+            msg_record_deleted = T("Example deleted"),
+            msg_list_empty = T("No Examples currently registered"),
+        )
+
+        # ---------------------------------------------------------------------
+        # Consent Option
+        # - a description of the data processing consent is requested for
+        # - multiple consecutive versions of a description for the same
+        #   type of data processing can exist, but once a user has consented
+        #   to a particular version of the description, it becomes a legal
+        #   document that must not be changed or deleted
+        #
+        tablename = "auth_consent_option"
+        define_table(tablename,
+                     Field("type_id", "reference auth_consent_option_type",
+                           # TODO requires
+                           # TODO represent
+                           ),
+                     s3_date("valid_from",
+                             default = "now",
+                             ),
+                     s3_date("valid_until"),
+                     Field("obsolete", "boolean",
+                           default = False,
+                           ),
+                     Field("name",
+                           requires = IS_NOT_EMPTY(),
+                           ),
+                     Field("explanation", "text",
+                           represent = s3_text_represent,
+                           ),
+                     s3_comments(),
+                     *s3_meta_fields())
+
+        # TODO CRUD Strings
+        crud_strings[tablename] = Storage(
+            label_create = T("Create Example"),
+            title_display = T("Example Details"),
+            title_list = T("Examples"),
+            title_update = T("Edit Example"),
+            label_list_button = T("List Examples"),
+            label_delete_button = T("Delete Example"),
+            msg_record_created = T("Example created"),
+            msg_record_modified = T("Example updated"),
+            msg_record_deleted = T("Example deleted"),
+            msg_list_empty = T("No Examples currently registered"),
+        )
+
+        # ---------------------------------------------------------------------
+        # Consent Question Responses
+        #
+        tablename = "auth_consent"
+        define_table(tablename,
+                     self.pr_person_id(),
+                     Field("vsign"),
+                     Field("vhash", "text"),
+                     Field("option_id", "reference auth_consent_option.id",
+                           ondelete = "RESTRICT",
+                           ),
+                     Field("consenting", "boolean",
+                           default = False,
+                           ),
+                     s3_date(default = "now",
+                             ),
+                     s3_date("expires_on"),
+                     *s3_meta_fields())
+
+        # ---------------------------------------------------------------------
+        # Pass names back to global scope (s3.*)
+        #
         return {}
 
 # =============================================================================
