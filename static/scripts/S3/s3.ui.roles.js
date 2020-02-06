79643c95df199542e3a3cd0ef78f68b86da5c2e3
Dominic König <dominic@nursix.org>
2020-02-06 12:38:42 +0100

    Bugfix role manager script

--- static/scripts/S3/s3.ui.roles.js
+++ static/scripts/S3/s3.ui.roles.js
@@ -341,7 +341,7 @@
                 }
             }
             itemList.sort(function(a, b) {
-                return a[1] === b[1] && 0 || (a[1] > b[1] && 1 || -1);
+                return a[1] !== b[1] && (a[1] > b[1] && 1 || -1) || 0;
             }).forEach(function(o) {
                 $('<option>').attr('value', o[0]).text(o[1]).appendTo(selector);
             });
