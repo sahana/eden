1fca2692bc65af56e5aee79abe2d6bfc6c0788c3
Dominic König <dominic@nursix.org>
2020-03-20 21:31:55 +0100

    Travis: switch to Bionic, drop PG-9.6, add PG-11 instead

--- tests/travis/configure_db.sh
+++ tests/travis/configure_db.sh
@@ -13,21 +13,21 @@ if [[ $DB == mysql ]]; then
 
 elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
 
-    if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.6 ]]; then
-        echo "Setting up postgres version 9.6"
-        sudo /etc/init.d/postgresql stop
-        sudo /etc/init.d/postgresql start 9.6
-        psql -c "create database sahana;" -U postgres
-
-        if [[ $DB == postgres-9.6+postgis ]]; then
-            sudo apt-get -qq update
-            sudo apt-get install -y postgresql-9.6-postgis-2.4
-            psql -U postgres -d sahana -c "create extension postgis"
-            psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
-            psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
-            sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
-        fi
-    fi
+    #if [[ `echo $DB | cut -f1 -d"+"` == postgres-9.6 ]]; then
+    #    echo "Setting up postgres version 9.6"
+    #    sudo /etc/init.d/postgresql stop
+    #    sudo /etc/init.d/postgresql start 9.6
+    #    psql -c "create database sahana;" -U postgres
+    #
+    #    if [[ $DB == postgres-9.6+postgis ]]; then
+    #        sudo apt-get -qq update
+    #        sudo apt-get install -y postgresql-9.6-postgis-2.4
+    #        psql -U postgres -d sahana -c "create extension postgis"
+    #        psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
+    #        psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
+    #        sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
+    #    fi
+    #fi
 
     if [[ `echo $DB | cut -f1 -d"+"` == postgres-10 ]]; then
         echo "Setting up postgres version 10"
@@ -46,22 +46,22 @@ elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
         fi
     fi
 
-    #if [[ `echo $DB | cut -f1 -d"+"` == postgres-11 ]]; then
-    #    echo "Setting up postgres version 11"
-    #    sudo /etc/init.d/postgresql stop
-    #    sudo /etc/init.d/postgresql start 11
-    #    psql -c "create database sahana;" -U postgres
-    #
-    #    if [[ $DB == postgres-11+postgis ]]; then
-    #        sudo apt-get -qq update
-    #        sudo apt-get install -y postgresql-11-postgis-2.5
-    #        sudo apt-get install -y postgresql-11-postgis-2.5-scripts
-    #        psql -U postgres -d sahana -c "create extension postgis"
-    #        psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
-    #        psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
-    #        sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
-    #    fi
-    #fi
+    if [[ `echo $DB | cut -f1 -d"+"` == postgres-11 ]]; then
+       echo "Setting up postgres version 11"
+       sudo /etc/init.d/postgresql stop
+       sudo /etc/init.d/postgresql start 11
+       psql -c "create database sahana;" -U postgres
+
+       if [[ $DB == postgres-11+postgis ]]; then
+           #sudo apt-get -qq update
+           #sudo apt-get install -y postgresql-11-postgis-2.5
+           #sudo apt-get install -y postgresql-11-postgis-2.5-scripts
+           psql -U postgres -d sahana -c "create extension postgis"
+           psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
+           psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
+           sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
+       fi
+    fi
 
     sed -ie 's|\#settings.database.db_type = "postgres"|settings.database.db_type = "postgres"|' models/000_config.py
     sed -ie 's|\#settings.database.username = "sahana"|settings.database.username = "travis"|' models/000_config.py
