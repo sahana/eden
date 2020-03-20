1684b01c1c389800dfac1a676ed6368a26456124
Dominic König <dominic@nursix.org>
2020-03-20 19:46:23 +0100

    Another attempt to run PG10+PostGIS-2.5 on Travis CI

--- tests/travis/configure_db.sh
+++ tests/travis/configure_db.sh
@@ -36,9 +36,9 @@ elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
         psql -c "create database sahana;" -U postgres
 
         if [[ $DB == postgres-10+postgis ]]; then
-            sudo apt-get -qq update
-            sudo apt-get install -y postgresql-10-postgis-2.5
-            sudo apt-get install -y postgresql-10-postgis-2.5-scripts
+            #sudo apt-get -qq update
+            #sudo apt-get install -y postgresql-10-postgis-2.5
+            #sudo apt-get install -y postgresql-10-postgis-2.5-scripts
             psql -U postgres -d sahana -c "create extension postgis"
             psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
             psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
@@ -46,22 +46,22 @@ elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
         fi
     fi
 
-    if [[ `echo $DB | cut -f1 -d"+"` == postgres-11 ]]; then
-        echo "Setting up postgres version 11"
-        sudo /etc/init.d/postgresql stop
-        sudo /etc/init.d/postgresql start 11
-        psql -c "create database sahana;" -U postgres
-
-        if [[ $DB == postgres-11+postgis ]]; then
-            sudo apt-get -qq update
-            sudo apt-get install -y postgresql-11-postgis-2.5
-            sudo apt-get install -y postgresql-11-postgis-2.5-scripts
-            psql -U postgres -d sahana -c "create extension postgis"
-            psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
-            psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
-            sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
-        fi
-    fi
+    #if [[ `echo $DB | cut -f1 -d"+"` == postgres-11 ]]; then
+    #    echo "Setting up postgres version 11"
+    #    sudo /etc/init.d/postgresql stop
+    #    sudo /etc/init.d/postgresql start 11
+    #    psql -c "create database sahana;" -U postgres
+    #
+    #    if [[ $DB == postgres-11+postgis ]]; then
+    #        sudo apt-get -qq update
+    #        sudo apt-get install -y postgresql-11-postgis-2.5
+    #        sudo apt-get install -y postgresql-11-postgis-2.5-scripts
+    #        psql -U postgres -d sahana -c "create extension postgis"
+    #        psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
+    #        psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
+    #        sed -ie 's|\#settings.gis.spatialdb = True|settings.gis.spatialdb = True|' models/000_config.py
+    #    fi
+    #fi
 
     sed -ie 's|\#settings.database.db_type = "postgres"|settings.database.db_type = "postgres"|' models/000_config.py
     sed -ie 's|\#settings.database.username = "sahana"|settings.database.username = "travis"|' models/000_config.py
