ba62ea61f23d5ecfbd8f984a8894d1dd94828543
Dominic König <dominic@nursix.org>
2020-03-20 10:15:18 +0100

    Travis: try other PostGIS repo

--- tests/travis/configure_db.sh
+++ tests/travis/configure_db.sh
@@ -52,8 +52,11 @@ elif [[ `echo $DB | cut -f1 -d"-"` == postgres ]]; then
         psql -c "create database sahana;" -U postgres
 
         if [[ $DB == postgres-11+postgis ]]; then
+            sudo apt -y install gnupg2
+            wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+            echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
             sudo apt-get -qq update
-            sudo apt-get install -y postgresql-11-postgis-2.5
+            sudo apt-get install -y postgis postgresql-11-postgis-2.5
             psql -U postgres -d sahana -c "create extension postgis"
             psql -U postgres -q -d sahana -c "grant all on geometry_columns to travis;"
             psql -U postgres -q -d sahana -c "grant all on spatial_ref_sys to travis;"
