440f6d16f722c008b88025535e49ced529330f86
Fran Boon <fran@aidiq.com>
2020-03-23 18:47:08 +0000

    S3Import: Fix tuple-specified templates

--- modules/s3/s3import.py
+++ modules/s3/s3import.py
@@ -710,12 +710,10 @@ class S3Importer(S3Method):
         elif isinstance(template, (tuple, list)):
             args.extend(template[:-1])
             template = template[-1]
-            if os.path.splitext(template)[1] in KNOWN_SPREADSHEET_EXTENSIONS:
-                args.append(template)
-            else:
+            if os.path.splitext(template)[1] not in KNOWN_SPREADSHEET_EXTENSIONS:
                 # Assume CSV if no known spreadsheet extension found
                 template = "%s.csv" % template
-                args.append([self.controller, template])
+            args.append(template)
         else:
             template = None
         if template is not None:
