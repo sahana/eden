f16754368e1780b55ca1ce901339bbcf04b99ea5
Fran Boon <fran@aidiq.com>
2020-05-19 11:46:52 +0100

    Redirects to consent controllers in auth

--- controllers/auth.py
+++ controllers/auth.py
@@ -4,23 +4,46 @@
     Auth Controllers
 """
 
+# -----------------------------------------------------------------------------
 def user():
     "Defined in admin module, so redirect there"
 
     redirect(URL(c="admin", args=request.args, vars=request.vars))
 
+# -----------------------------------------------------------------------------
 def group():
     "Defined in admin module, so redirect there"
 
     redirect(URL(c="admin", args=request.args, vars=request.vars))
 
+# -----------------------------------------------------------------------------
 def membership():
     "Defined in admin module, so redirect there"
 
     redirect(URL(c="admin", args=request.args, vars=request.vars))
 
+# -----------------------------------------------------------------------------
 def domain():
     "Defined in admin module, so redirect there"
 
     redirect(URL(c="admin", args=request.args, vars=request.vars))
 
+# =============================================================================
+def consent():
+    "Defined in admin module, so redirect there"
+
+    redirect(URL(c="admin", args=request.args, vars=request.vars))
+
+# -----------------------------------------------------------------------------
+def consent_option():
+    "Defined in admin module, so redirect there"
+
+    redirect(URL(c="admin", args=request.args, vars=request.vars))
+
+# -----------------------------------------------------------------------------
+def processing_type():
+    "Defined in admin module, so redirect there"
+
+    redirect(URL(c="admin", args=request.args, vars=request.vars))
+
+# =============================================================================
