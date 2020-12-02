7d837d2cf1dc2d2e8526352d48df26300b0d714a
Dominic König <dominic@nursix.org>
2020-12-02 13:34:50 +0100

    RLP: Improved self-registration page style

--- modules/templates/RLP/controllers.py
+++ modules/templates/RLP/controllers.py
@@ -274,6 +274,7 @@ class register(S3CustomController):
 
         # Page title and intro text
         title = T("Volunteer Registration")
+        intro = T("Please fill in all relevant information for your deployment. To support the choice of suitable candidates we especially need information about your place of residence (postcode and town), your contact information (phone and email) and the weekly working time (hours per week) your are able to support.")
 
         # Form Fields
         formfields, required_fields, subheadings = self.formfields()
@@ -459,6 +460,7 @@ class register(S3CustomController):
         self._view(THEME, "register.html")
 
         return {"title": title,
+                "intro": intro,
                 "form": form,
                 }
 
@@ -638,8 +640,7 @@ class register(S3CustomController):
                            ]
 
         # Subheadings
-        subheadings = ((0, T("Please fill in all relevant information for your deployment. To support the choice of suitable candidates we especially need information about your place of residence (postcode and town), your contact information (phone and email) and the weekly working time (hours per week) your are able to support.")),
-                       (3, T("User Account")),
+        subheadings = ((3, T("User Account")),
                        (6, T("Contact Information")),
                        (8, T("Address")),
                        (11, T("Occupation")),
