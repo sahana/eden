77cc40825fc37e966f4966eb0809d5e1146ef72b
Fran Boon <fran@aidiq.com>
2021-07-12 19:55:54 +0100

    RMSAmericas: Progress with new theme

--- modules/templates/RMSAmericas/controllers.py
+++ modules/templates/RMSAmericas/controllers.py
@@ -3,6 +3,7 @@
 from os import path
 
 from gluon import *
+from gluon.storage import Storage
 
 from s3 import S3CustomController
 
@@ -62,6 +63,227 @@ class index(S3CustomController):
 
         self._view(THEME, "index.html")
         return output
+
+# =============================================================================
+class apps(S3CustomController):
+    """ App Switcher """
+
+    def __call__(self):
+
+        T = current.T
+        auth = current.auth
+        has_roles = auth.s3_has_roles
+        ORG_ADMIN = current.session.s3.system_roles.ORG_ADMIN
+
+        # Which apps are available for this user?
+        apps = []
+        apps_append = apps.append
+        _div = self.div
+
+        if has_roles((ORG_ADMIN,
+                      "hr_manager",
+                      "hr_assistant",
+                      "training_coordinator",
+                      "training_assistant",
+                      "surge_capacity_manager",
+                      "disaster_manager",
+                      )):
+            apps_append(_div(label = T("Human Talent"),
+                             url = URL(c = "hrm",
+                                       f = "index",
+                                       ),
+                             image = "human_talent.png",
+                             _class = "alh",
+                             ))
+
+        if has_roles((ORG_ADMIN,
+                      "training_coordinator",
+                      "training_assistant",
+                      "ns_training_manager",
+                      "ns_training_assistant",
+                      "surge_capacity_manager",
+                      "disaster_manager",
+                      )):
+            apps_append(_div(label = T("Training"),
+                             url = URL(c = "hrm",
+                                       f = "training_event",
+                                       ),
+                             image = "training.png",
+                             ))
+
+        if auth.permission.accessible_url(c = "member",
+                                          f = "membership",
+                                          ):
+            apps_append(_div(label = T("Partners"),
+                             url = URL(c = "member",
+                                       f = "membership",
+                                       ),
+                             image = "partners.png",
+                             ))
+
+        if has_roles((ORG_ADMIN,
+                      "wh_manager",
+                      "national_wh_manager",
+                      )):
+            apps_append(_div(label = T("Warehouses"),
+                             url = URL(c = "inv",
+                                       f = "index",
+                                       ),
+                             image = "warehouses.png",
+                             _class = "alw",
+                             ))
+
+        if has_roles(("project_reader",
+                      "project_manager",
+                      "monitoring_evaluation",
+                      )):
+            apps_append(_div(label = T("Projects"),
+                             url = URL(c = "project",
+                                       f = "project",
+                                       args = "summary",
+                                       ),
+                             image = "projects.png",
+                             ))
+
+        if has_roles(("surge_capacity_manager",
+                      "disaster_manager",
+                      )):
+            apps_append(_div(label = T("RIT"),
+                             url = URL(c = "deploy",
+                                       f = "mission",
+                                       args = "summary",
+                                       vars = {"status__belongs": 2},
+                                       ),
+                             image = "RIT.png",
+                             ))
+
+        # Layout the apps
+        len_apps = len(apps)
+        if len_apps == 0:
+            # No Apps available
+            # This generally gets caught earlier & user sees no App Switcher at all
+            resize = True
+            height = 112
+            width = 110
+            apps = DIV(_class = "row",
+                       )
+        elif len_apps == 1:
+            # 1x1
+            resize = True
+            height = 112
+            width = 110
+            apps[0]["_class"] = "small-12 columns"
+            apps = DIV(apps[0],
+                       _class = "row",
+                       )
+        elif len_apps == 2:
+            # 1x2
+            resize = True
+            height = 112
+            width = 220
+            apps[0]["_class"] = "small-6 columns"
+            apps[1]["_class"] = "small-6 columns"
+            apps = DIV(apps[0],
+                       apps[1],
+                       _class = "row",
+                       )
+        elif len_apps == 3:
+            # 2x2
+            resize = True
+            height = 224
+            width = 220
+            apps[0]["_class"] = "small-6 columns"
+            apps[1]["_class"] = "small-6 columns"
+            apps[2]["_class"] = "small-6 columns"
+            apps = TAG[""](DIV(apps[0],
+                               apps[1],
+                               _class = "row",
+                               ),
+                           DIV(apps[2],
+                               _class = "row",
+                               ),
+                           )
+        elif len_apps == 4:
+            # 2x2
+            resize = True
+            height = 224
+            width = 220
+            apps[0]["_class"] = "small-6 columns"
+            apps[1]["_class"] = "small-6 columns"
+            apps[2]["_class"] = "small-6 columns"
+            apps[3]["_class"] = "small-6 columns"
+            apps = TAG[""](DIV(apps[0],
+                               apps[1],
+                               _class = "row",
+                               ),
+                           DIV(apps[2],
+                               apps[3],
+                               _class = "row",
+                               ),
+                           )
+        else:
+            # 2xX
+            resize = False
+            row2 = DIV(apps[3],
+                       apps[4],
+                       _class = "row",
+                       )
+            if len_apps == 6:
+                row2.append(apps[5])
+            apps = TAG[""](DIV(apps[0],
+                               apps[1],
+                               apps[2],
+                               _class = "row",
+                               ),
+                           row2,
+                           )
+
+        output = {"apps": apps,
+                  }
+
+        if resize:
+            # Insert JS to resize the parent iframe
+            output["js"] = '''window.parent.$('#apps-frame').parent().height(%s).width(%s)''' % \
+                                (height, width)
+
+        self._view(THEME, "apps.html")
+        return output
+
+    # -------------------------------------------------------------------------
+    @staticmethod
+    def div(label,
+            url,
+            image,
+            _class = None
+            ):
+
+        if _class:
+            #Extra class on label's span to fit it in better
+            _class = "al %s" % _class
+        else:
+            _class = "al"
+
+        div = DIV(DIV(A(IMG(_src = URL(c="static", f="themes",
+                                       args = [THEME,
+                                               "img",
+                                               image,
+                                               ]),
+                            _class = "ai",
+                            _height = "64",
+                            _width = "64",
+                            ),
+                        SPAN(label,
+                             _class = _class,
+                             ),
+                        _href = url,
+                        _target = "_top",
+                        ),
+                      _class = "ah",
+                      ),
+                  _class = "small-4 columns",
+                  )
+
+        return div
 
 # =============================================================================
 def deploy_index():
--- modules/templates/RMSAmericas/layouts.py
+++ modules/templates/RMSAmericas/layouts.py
@@ -32,16 +32,129 @@ class S3MainMenuLayout(S3NavigationItem):
         """ Custom Layout Method """
 
         T = current.T
+        auth = current.auth
+        has_role = auth.s3_has_role
+
+        settings = ""
+
+        len_roles = len(current.session.s3.roles)
+        if (len_roles <= 2) or \
+           (len_roles == 3 and has_role("RIT_MEMBER", include_admin=False)):
+            # No specific Roles
+            # Just show Profile on main menu
+            apps = ""
+            iframe = ""
+        else:
+            # Show Applications switcher
+            current.response.s3.scripts.append("/%s/static/themes/RMSAmericas/js/nav.js" % current.request.application)
+            apps = DIV(A(SVG(PATH(_d = "M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z",
+                                  ),
+                             _fill = "#5f6368",
+                             _height = "24px",
+                             _width = "24px",
+                             ),
+                         _href = "#",
+                         _role = "button",
+                         _title = T("RMS modules"),
+                         ),
+                       _class = "hd",
+                       _id = "apps-btn",
+                       )
+
+            iframe = DIV(IFRAME(_role = "presentation",
+                                _class = "hide",
+                                _id = "apps-frame",
+                                _frameborder = "0",
+                                _scrolling = "no",
+                                _src = URL(c="default", f="index",
+                                           args = "apps",
+                                           ),
+                                _style = "height: 100%; width: 100%;",
+                                ),
+                         _style = "overflow: hidden; position: absolute; top: 0px; width: 328px; z-index: 991; height: 224px; margin-top: 250px; transition: height 0.3s ease-in-out 0s; right: 0px; margin-right: 4px; max-height: calc(-65px + 100vh);"
+                         )
+            if has_role("ADMIN"):
+                settings = URL(c="admin", f="index")
+            elif has_role("ORG_ADMIN"):
+                settings = URL(c="admin", f="user")
+            elif auth.s3_has_roles(("hr_manager",
+                                    "ns_training_manager",
+                                    "training_coordinator",
+                                    )):
+                settings = URL(c="pr", f="forum")
+            #elif auth.s3_has_roles(("wh_manager",
+            #                        )):
+            #   # @ToDo: WMS Module configuration
+            #            ▪ Labelling
+            #            ▪ Auto localisation
+            #            ▪ Sharing authorisation
+            #            ▪ Alerts
+            #            ▪ Email for notification    
+            if settings:
+                settings = DIV(A(SVG(PATH(_d = "M13.85 22.25h-3.7c-.74 0-1.36-.54-1.45-1.27l-.27-1.89c-.27-.14-.53-.29-.79-.46l-1.8.72c-.7.26-1.47-.03-1.81-.65L2.2 15.53c-.35-.66-.2-1.44.36-1.88l1.53-1.19c-.01-.15-.02-.3-.02-.46 0-.15.01-.31.02-.46l-1.52-1.19c-.59-.45-.74-1.26-.37-1.88l1.85-3.19c.34-.62 1.11-.9 1.79-.63l1.81.73c.26-.17.52-.32.78-.46l.27-1.91c.09-.7.71-1.25 1.44-1.25h3.7c.74 0 1.36.54 1.45 1.27l.27 1.89c.27.14.53.29.79.46l1.8-.72c.71-.26 1.48.03 1.82.65l1.84 3.18c.36.66.2 1.44-.36 1.88l-1.52 1.19c.01.15.02.3.02.46s-.01.31-.02.46l1.52 1.19c.56.45.72 1.23.37 1.86l-1.86 3.22c-.34.62-1.11.9-1.8.63l-1.8-.72c-.26.17-.52.32-.78.46l-.27 1.91c-.1.68-.72 1.22-1.46 1.22zm-3.23-2h2.76l.37-2.55.53-.22c.44-.18.88-.44 1.34-.78l.45-.34 2.38.96 1.38-2.4-2.03-1.58.07-.56c.03-.26.06-.51.06-.78s-.03-.53-.06-.78l-.07-.56 2.03-1.58-1.39-2.4-2.39.96-.45-.35c-.42-.32-.87-.58-1.33-.77l-.52-.22-.37-2.55h-2.76l-.37 2.55-.53.21c-.44.19-.88.44-1.34.79l-.45.33-2.38-.95-1.39 2.39 2.03 1.58-.07.56a7 7 0 0 0-.06.79c0 .26.02.53.06.78l.07.56-2.03 1.58 1.38 2.4 2.39-.96.45.35c.43.33.86.58 1.33.77l.53.22.38 2.55z",
+                                          ),
+                                     CIRCLE(_cx = "12",
+                                            _cy = "12",
+                                            _r = "3.5",
+                                            ),
+                                     _fill = "#5f6368",
+                                     _height = "24px",
+                                     _width = "24px",
+                                     ),
+                                 _href = settings,
+                                 _role = "button",
+                                 _title = T("Settings"),
+                                 ),
+                               _class = "hd",
+                               )
+
+        user_a = A(s3_avatar_represent(auth.user.id,
+                                       _class = "ip",
+                                       _height = 36,
+                                       _width = 36,
+                                       ),
+                   _role = "button",
+                   _title = T("RMS Account"),
+                   )
+        user_a["_data-dropdown"] = "user_drop"
+        user_a["_aria-controls"] = "user_drop"
+        user_a["_aria-expanded"] = "false"
+        user_menu = UL(LI(A(T("Profile"),
+                            _href = URL(c="hrm", f="person",
+                                        args = [str(auth.s3_logged_in_person())],
+                                        vars = {"profile":1},
+                                        ),
+                            ),
+                          ),
+                       LI(A(T("Change Password"),
+                            _href = URL(c="default", f="user",
+                                        args = "change_password",
+                                        ),
+                            ),
+                          ),
+                       LI(A(T("Logout"),
+                            _href = URL(c="default", f="user",
+                                        args = "logout",
+                                        ),
+                            ),
+                          ),
+                       _id = "user_drop",
+                       _class = "f-dropdown",
+                       _tabindex = "-1",
+                       )
+        user_menu["data-dropdown-content"]
+        user_menu["aria-hidden"] = "true"
 
         divs = [DIV(DIV(A(SVG(PATH(_d = "M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z",
                                    ),
                               _fill = "#5f6368",
+                              _height = "24px",
+                              _width = "24px",
                               ),
-                          _href = "#",
-                          _class = "ha",
                           _role = "button",
                           _title = T("Main menu"),
                           ),
+                        _id = "menu-btn",
                         _class = "hd",
                         ),
                     _class = "large-2 medium-3 small-4 columns",
@@ -54,54 +167,24 @@ class S3MainMenuLayout(S3NavigationItem):
                               PATH(_d = "M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z",
                                    ),
                               _fill = "#5f6368",
+                              _height = "24px",
+                              _width = "24px",
                               ),
                           _href = URL(c = "default",
                                       f = "help",
                                       ),
-                          _class = "ha",
                           _role = "button",
                           _title = T("Support"),
                           ),
                         _class = "hd",
                         ),
-                    DIV(A(SVG(PATH(_d = "M13.85 22.25h-3.7c-.74 0-1.36-.54-1.45-1.27l-.27-1.89c-.27-.14-.53-.29-.79-.46l-1.8.72c-.7.26-1.47-.03-1.81-.65L2.2 15.53c-.35-.66-.2-1.44.36-1.88l1.53-1.19c-.01-.15-.02-.3-.02-.46 0-.15.01-.31.02-.46l-1.52-1.19c-.59-.45-.74-1.26-.37-1.88l1.85-3.19c.34-.62 1.11-.9 1.79-.63l1.81.73c.26-.17.52-.32.78-.46l.27-1.91c.09-.7.71-1.25 1.44-1.25h3.7c.74 0 1.36.54 1.45 1.27l.27 1.89c.27.14.53.29.79.46l1.8-.72c.71-.26 1.48.03 1.82.65l1.84 3.18c.36.66.2 1.44-.36 1.88l-1.52 1.19c.01.15.02.3.02.46s-.01.31-.02.46l1.52 1.19c.56.45.72 1.23.37 1.86l-1.86 3.22c-.34.62-1.11.9-1.8.63l-1.8-.72c-.26.17-.52.32-.78.46l-.27 1.91c-.1.68-.72 1.22-1.46 1.22zm-3.23-2h2.76l.37-2.55.53-.22c.44-.18.88-.44 1.34-.78l.45-.34 2.38.96 1.38-2.4-2.03-1.58.07-.56c.03-.26.06-.51.06-.78s-.03-.53-.06-.78l-.07-.56 2.03-1.58-1.39-2.4-2.39.96-.45-.35c-.42-.32-.87-.58-1.33-.77l-.52-.22-.37-2.55h-2.76l-.37 2.55-.53.21c-.44.19-.88.44-1.34.79l-.45.33-2.38-.95-1.39 2.39 2.03 1.58-.07.56a7 7 0 0 0-.06.79c0 .26.02.53.06.78l.07.56-2.03 1.58 1.38 2.4 2.39-.96.45.35c.43.33.86.58 1.33.77l.53.22.38 2.55z",
-                                   ),
-                              CIRCLE(_cx = "12",
-                                     _cy = "12",
-                                     _r = "3.5",
-                                     ),
-                              _fill = "#5f6368",
-                              ),
-                          _href = "#",
-                          _class = "ha",
-                          _role = "button",
-                          _title = T("Settings"),
-                          ),
-                        _class = "hd",
-                        ),
-                    DIV(A(SVG(PATH(_d = "M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z",
-                                   ),
-                              _fill = "#5f6368",
-                              ),
-                          _href = "#",
-                          _class = "ha",
-                          _role = "button",
-                          _title = T("RMS modules"),
-                          ),
-                        _class = "hd",
-                        ),
-                    DIV(A(s3_avatar_represent(current.auth.user.id,
-                                              _class = "ip",
-                                              _height = 36,
-                                              _width = 36,
-                                              ),
-                          _href = "#",
-                          _class = "hap",
-                          _role = "button",
-                          _title = T("RMS Account"),
-                          ),
+                    settings,
+                    apps,
+                    DIV(user_a,
+                        user_menu,
                         _class = "hdp",
                         ),
+                    iframe,
                     _class = "large-2 medium-3 small-4 columns",
                     ),
                 ]
